a4acd3019127da119e364c6da140ca272be4048f
yaobin-khb <51393259+krihy@users.noreply.github.com>
2023-08-30 17:00:17 +0800

    refactor(osc): refactor rename table (#65)
    * reactor oms client
    
    * 1.add osc expired time when exceed oscTaskExpiredAfterSeconds
    2.release oms project before start next schedule task
    3.set current schedule task id quartz data map
    and reschedule job before start next
    
    * refactor osc swap table
    
    * oms replaced by OceanBase Migration Service (OMS) in ErrorMessages.properties
    
    * add OmsDataCheckInconsistent in ErrorCodes
    
    * rename method name ContinueHandleProjectStepResult
    
    * 1.start and update schedule trigger by trigger data map
    
    * 1.format code

--- server/odc-service/src/main/java/com/oceanbase/odc/service/onlineschemachange/pipeline/SwapTableNameValve.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/onlineschemachange/pipeline/SwapTableNameValve.java
@@ -15,13 +15,8 @@
  */
 package com.oceanbase.odc.service.onlineschemachange.pipeline;
 
-import java.text.MessageFormat;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 import com.oceanbase.odc.core.session.ConnectionSession;
 import com.oceanbase.odc.core.session.ConnectionSessionUtil;
@@ -29,16 +24,11 @@ import com.oceanbase.odc.core.shared.PreConditions;
 import com.oceanbase.odc.metadb.schedule.ScheduleTaskEntity;
 import com.oceanbase.odc.service.connection.ConnectionService;
 import com.oceanbase.odc.service.connection.model.ConnectionConfig;
-import com.oceanbase.odc.service.db.browser.DBSchemaAccessors;
 import com.oceanbase.odc.service.onlineschemachange.model.OnlineSchemaChangeParameters;
 import com.oceanbase.odc.service.onlineschemachange.model.OnlineSchemaChangeScheduleTaskParameters;
 import com.oceanbase.odc.service.onlineschemachange.rename.DefaultRenameTableInvoker;
-import com.oceanbase.odc.service.onlineschemachange.rename.RenameTableHandler;
-import com.oceanbase.odc.service.onlineschemachange.rename.RenameTableHandlers;
-import com.oceanbase.odc.service.onlineschemachange.rename.RenameTableParameters;
 import com.oceanbase.odc.service.session.DBSessionManageFacade;
 import com.oceanbase.odc.service.session.factory.DefaultConnectSessionFactory;
-import com.oceanbase.tools.dbbrowser.schema.DBSchemaAccessor;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -68,117 +58,17 @@ public class SwapTableNameValve extends BaseValve {
         OnlineSchemaChangeParameters parameters = context.getParameter();
 
         ConnectionConfig config = context.getConnectionConfig();
-        RenameTableHandler renameTableExecutor = null;
         ConnectionSession connectionSession = new DefaultConnectSessionFactory(config).generateSession();
-        ConnectionSessionUtil.setCurrentSchema(connectionSession, taskParameters.getDatabaseName());
-
         try {
-            renameTableExecutor = RenameTableHandlers.getForeignKeyHandler(connectionSession);
-
-            swapTable(taskParameters, parameters, renameTableExecutor, connectionSession);
-
+            ConnectionSessionUtil.setCurrentSchema(connectionSession, taskParameters.getDatabaseName());
+            DefaultRenameTableInvoker defaultRenameTableInvoker =
+                    new DefaultRenameTableInvoker(connectionSession, dbSessionManageFacade);
+            defaultRenameTableInvoker.invoke(taskParameters, parameters);
             context.setSwapSucceedCallBack(true);
         } finally {
-            cleanUp(connectionSession, taskParameters, renameTableExecutor);
+            connectionSession.expire();
         }
     }
-
-    private void swapTable(OnlineSchemaChangeScheduleTaskParameters taskParameters,
-            OnlineSchemaChangeParameters parameters, RenameTableHandler renameTableHandler,
-            ConnectionSession connSession) {
-        DefaultRenameTableInvoker renameTableInvoker = new DefaultRenameTableInvoker(connSession,
-                dbSessionManageFacade);
-        renameTableInvoker.invoke(
-                () -> {
-                    retryRename(taskParameters, parameters, renameTableHandler, connSession);
-                    return null;
-                },
-                getRenameTableParameters(taskParameters, parameters));
-    }
-
-    private RenameTableParameters getRenameTableParameters(OnlineSchemaChangeScheduleTaskParameters taskParameters,
-            OnlineSchemaChangeParameters parameters) {
-        // set lock table max timeout is 120s
-        Integer lockTableTimeOutSeconds =
-                Math.min(parameters.getLockTableTimeOutSeconds() == null ? 15 : parameters.getLockTableTimeOutSeconds(),
-                        120);
-
-        return RenameTableParameters.builder()
-                .schemaName(taskParameters.getDatabaseName())
-                .renamedTableName(taskParameters.getRenamedTableName())
-                .originTableName(taskParameters.getOriginTableName())
-                .newTableName(taskParameters.getNewTableName())
-                .lockTableTimeOutSeconds(lockTableTimeOutSeconds)
-                .build();
-
-    }
-
-    private void retryRename(OnlineSchemaChangeScheduleTaskParameters taskParameters,
-            OnlineSchemaChangeParameters parameters, RenameTableHandler renameTableHandler,
-            ConnectionSession connectionSession) {
-
-        Integer swapTableNameRetryTimes = parameters.getSwapTableNameRetryTimes();
-        if (swapTableNameRetryTimes == 0) {
-            swapTableNameRetryTimes = 1;
-        }
-
-        AtomicInteger retryTime = new AtomicInteger();
-        boolean succeed = false;
-
-        while (retryTime.getAndIncrement() < swapTableNameRetryTimes) {
-            try {
-                renameTableHandler.rename(taskParameters.getDatabaseName(), taskParameters.getOriginTableName(),
-                        taskParameters.getRenamedTableName(), taskParameters.getNewTableName());
-                succeed = true;
-                break;
-            } catch (Exception e) {
-                log.warn(MessageFormat.format("Swap table name occur error, retry time {0}",
-                        retryTime.get()), e);
-                renameBack(connectionSession, taskParameters, renameTableHandler);
-            }
-        }
-        if (!succeed) {
-            throw new IllegalStateException(
-                    MessageFormat.format("Swap table name failed after {0} times", retryTime.get()));
-        } else {
-            log.info("Table has successfully swapped, new schema takes effect now");
-        }
-    }
-
-    private void cleanUp(ConnectionSession connectionSession,
-            OnlineSchemaChangeScheduleTaskParameters taskParameters,
-            RenameTableHandler renameTableExecutor) {
-        renameBack(connectionSession, taskParameters, renameTableExecutor);
-        connectionSession.expire();
-    }
-
-    private void renameBack(ConnectionSession connectionSession,
-            OnlineSchemaChangeScheduleTaskParameters taskParameters, RenameTableHandler renameTableExecutor) {
-
-        /*
-         * If the original table was successfully renamed to _old but the second rename operation failed,
-         * rollback the first renaming
-         */
-        try {
-            DBSchemaAccessor dbSchemaAccessor = DBSchemaAccessors.create(connectionSession);
-            List<String> renamedTable = dbSchemaAccessor.showTablesLike(taskParameters.getDatabaseName(),
-                    taskParameters.getRenamedTableName());
-
-            List<String> originTable = dbSchemaAccessor.showTablesLike(taskParameters.getDatabaseName(),
-                    taskParameters.getOriginTableNameUnWrapped());
-
-            if (!CollectionUtils.isEmpty(renamedTable) && CollectionUtils.isEmpty(originTable)) {
-
-                renameTableExecutor.rename(taskParameters.getDatabaseName(),
-                        taskParameters.getRenamedTableName(), taskParameters.getOriginTableName());
-                log.info("Because previous swap occur error, so we rename back, renamed {} TO {}",
-                        taskParameters.getRenamedTableNameWithSchema(), taskParameters.getOriginTableNameWithSchema());
-            }
-        } catch (Exception exception) {
-            log.warn("Rename back occur error", exception);
-        }
-    }
-
 }
 
 
