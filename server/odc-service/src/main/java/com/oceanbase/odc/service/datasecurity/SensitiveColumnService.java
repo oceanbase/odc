045ff9255b4c410e1205fd4f1e440d5cd4e75d31
XiaoYang <gaoda.xy@oceanbase.com>
2023-09-06 20:23:54 +0800

    fix(data-security): create sensitive columns with case insensitive same column and table names (#175)
    * fix: create duplicated sensitive columns
    
    * fix: import sort
    
    * fix: scan duplicated sensitive columns
    
    * add: unit test

--- server/odc-service/src/main/java/com/oceanbase/odc/service/datasecurity/SensitiveColumnService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/datasecurity/SensitiveColumnService.java
@@ -70,7 +70,6 @@ import com.oceanbase.odc.service.iam.UserService;
 import com.oceanbase.odc.service.iam.auth.AuthenticationFacade;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -430,7 +429,6 @@ public class SensitiveColumnService {
     }
 
     @Data
-    @EqualsAndHashCode
     private static class SensitiveColumnMeta {
         private Long databaseId;
         private String tableName;
@@ -441,6 +439,23 @@ public class SensitiveColumnService {
             this.tableName = entity.getTableName();
             this.columnName = entity.getColumnName();
         }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(databaseId, tableName.toLowerCase(), columnName.toLowerCase());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof SensitiveColumnMeta) {
+                SensitiveColumnMeta other = (SensitiveColumnMeta) obj;
+                return Objects.equals(databaseId, other.databaseId)
+                        && Objects.equals(tableName.toLowerCase(), other.tableName.toLowerCase())
+                        && Objects.equals(columnName.toLowerCase(), other.columnName.toLowerCase());
+            }
+            return false;
+        }
+
     }
 
 }
