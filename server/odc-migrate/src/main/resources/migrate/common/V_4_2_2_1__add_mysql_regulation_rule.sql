34f5cd0edd7485d4cdefe5b723034e992ae5c722
jingtian <jingtian.zx@oceanbase.com>
2023-09-07 15:44:16 +0800

    ODP sharding mysql adaption for security regulation

--- server/odc-migrate/src/main/resources/migrate/common/V_4_2_2_1__add_mysql_regulation_rule.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_2_2_1__add_mysql_regulation_rule.sql
@@ -1,9 +1,56 @@
-UPDATE regulation_rule_applying rra
-SET rra.applied_dialect_types = CONCAT(SUBSTRING(rra.applied_dialect_types, 1, LENGTH(rra.applied_dialect_types) - 1), ',"MYSQL"]')
-where rra.rule_metadata_id in (
-    select id from regulation_rule_metadata rrm where rrm.name not in (
+UPDATE
+  regulation_rule_applying
+SET
+  applied_dialect_types = IF(
+    applied_dialect_types = '[]',
+    '["MYSQL"]',
+    CONCAT(
+      SUBSTRING(
+        applied_dialect_types,
+        1,
+        LENGTH(applied_dialect_types) - 1
+      ),
+      ',"MYSQL"]'
+    )
+  )
+where
+  rule_metadata_id in (
+    select
+      id
+    from
+      regulation_rule_metadata rrm
+    where
+      rrm.name not in (
         '${com.oceanbase.odc.builtin-resource.regulation.rule.sql-check.prefer-local-index.name}',
         '${com.oceanbase.odc.builtin-resource.regulation.rule.sql-check.restrict-column-name-case.name}',
         '${com.oceanbase.odc.builtin-resource.regulation.rule.sql-check.restrict-table-name-case.name}'
+      )
+  );
+
+UPDATE
+  regulation_rule_applying
+SET
+  applied_dialect_types = IF(
+    applied_dialect_types = '[]',
+    '["ODP_SHARDING_OB_MYSQL"]',
+    CONCAT(
+      SUBSTRING(
+        applied_dialect_types,
+        1,
+        LENGTH(applied_dialect_types) - 1
+      ),
+      ',"ODP_SHARDING_OB_MYSQL"]'
     )
-);
+  )
+where
+  rule_metadata_id in (
+    select
+      id
+    from
+      regulation_rule_metadata rrm
+    where
+      rrm.name not in (
+        '${com.oceanbase.odc.builtin-resource.regulation.rule.sql-check.restrict-column-name-case.name}',
+        '${com.oceanbase.odc.builtin-resource.regulation.rule.sql-check.restrict-table-name-case.name}'
+      )
+  );
