a1d9416ccf076676231db4d34bae1d2803af6972
pynzzZ <39962741+MarkPotato777@users.noreply.github.com>
2023-09-09 20:08:08 +0800

    refactor(migration): extract data migration interface (#290)
    * test
    
    * refactor
    
    * optimize datasource sync
    
    * response to comments
    
    * fix
    
    * refactor
    
    * fix
    
    * fix
    
    * fix
    
    * rollback sync

--- server/odc-service/src/main/java/com/oceanbase/odc/service/collaboration/TeamOrganizationMigrator.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/collaboration/TeamOrganizationMigrator.java
@@ -116,8 +116,8 @@ public class TeamOrganizationMigrator {
         parameters.putIfAbsent(ResourceConstants.CREATOR_ID_PLACEHOLDER_NAME, userId);
         List<String> resourceLocations = new LinkedList<>();
         resourceLocations.add("migrate/rbac/V_3_2_0_5__iam_role.yaml");
-        resourceLocations.add("migrate/rbac/V_4_2_0_24__resource_role.yaml");
-        resourceLocations.add("init-config/regulation");
+        resourceLocations.add("migrate/common/V_4_2_0_24__resource_role.yaml");
+        resourceLocations.add("runtime");
 
         return ResourceConfig.builder()
                 .dataSource(dataSource)
--- server/odc-service/src/main/java/com/oceanbase/odc/service/connection/database/DatabaseSyncManager.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/connection/database/DatabaseSyncManager.java
@@ -25,6 +25,8 @@ import org.springframework.stereotype.Service;
 
 import com.oceanbase.odc.core.authority.util.SkipAuthorize;
 import com.oceanbase.odc.core.shared.exception.BadRequestException;
+import com.oceanbase.odc.service.connection.model.ConnectionConfig;
+import com.oceanbase.odc.service.iam.util.SecurityContextUtils;
 
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -45,8 +47,11 @@ public class DatabaseSyncManager {
     private ThreadPoolTaskExecutor executor;
 
     @SkipAuthorize("internal usage")
-    public Future<Boolean> submitSyncDataSourceTask(@NonNull Long dataSourceId) {
-        return doExecute(() -> executor.submit(() -> databaseService.internalSyncDataSourceSchemas(dataSourceId)));
+    public Future<Boolean> submitSyncDataSourceTask(@NonNull ConnectionConfig connection) {
+        return doExecute(() -> executor.submit(() -> {
+            SecurityContextUtils.setCurrentUser(connection.getCreatorId(), connection.getOrganizationId(), null);
+            return databaseService.internalSyncDataSourceSchemas(connection.getId());
+        }));
     }
 
     @SkipAuthorize("internal usage")
