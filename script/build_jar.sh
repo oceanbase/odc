9900b6cbed563c05f96275e306fbbec41b59d5ba
yizhou <yizhou.xw@gmail.com>
2023-10-17 16:38:08 +0800

    build: split libs build due time costing

--- script/build_jar.sh
+++ script/build_jar.sh
@@ -7,11 +7,6 @@ if ! source $(dirname "$0")/functions.sh; then
     exit 1
 fi
 
-if ! maven_install_libs; then
-    echo "maven install libs failed"
-    exit 2
-fi
-
 if ! maven_build_jar; then
     echo "maven build jar failed"
     exit 3
