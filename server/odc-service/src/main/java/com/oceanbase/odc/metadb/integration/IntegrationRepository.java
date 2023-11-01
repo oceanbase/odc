da8739887a406f9e14bf4d7c1502481ac862839d
ungreat <ungreatang@gmail.com>
2023-11-01 18:20:38 +0800

    ssoTestLogin

--- server/odc-service/src/main/java/com/oceanbase/odc/metadb/integration/IntegrationRepository.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/metadb/integration/IntegrationRepository.java
@@ -39,4 +39,6 @@ public interface IntegrationRepository extends OdcJpaRepository<IntegrationEntit
     List<IntegrationEntity> findByTypeAndEnabledAndOrganizationIdIn(IntegrationType type, Boolean enabled,
             Collection<Long> organizationIds);
 
+    Optional<IntegrationEntity> findByTypeAndOrganizationIdAndName(IntegrationType type, Long organizationId, String name);
+
 }
