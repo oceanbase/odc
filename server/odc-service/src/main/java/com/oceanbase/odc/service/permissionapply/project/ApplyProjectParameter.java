9d71a84df8b9061747a3a4c9f09caccd5d364c26
MarkPotato777 <yinuo.pyn@oceanbase.com>
2023-11-09 20:46:21 +0800

    fix apply project

--- server/odc-service/src/main/java/com/oceanbase/odc/service/permissionapply/project/ApplyProjectParameter.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/permissionapply/project/ApplyProjectParameter.java
@@ -20,7 +20,6 @@ import java.io.Serializable;
 import java.util.List;
 
 import com.oceanbase.odc.core.flow.model.TaskParameters;
-import com.oceanbase.odc.core.shared.constant.ResourceRoleName;
 import com.oceanbase.odc.service.collaboration.project.model.Project;
 import com.oceanbase.odc.service.iam.model.ResourceRole;
 
@@ -69,10 +68,6 @@ public class ApplyProjectParameter implements Serializable, TaskParameters {
          * Resource role id, refer to {@link ResourceRole#getId()}
          */
         private Long id;
-        /**
-         * Resource role name, refer to {@link ResourceRole#getRoleName()}
-         */
-        private ResourceRoleName name;
     }
 
 }
