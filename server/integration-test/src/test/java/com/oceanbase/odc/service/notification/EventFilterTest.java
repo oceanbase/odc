5d1419c1dd30e18152aefb796925bab8317bc15f
LuckyPickleZZ <lyzhuozz@gmail.com>
2023-11-18 11:21:31 +0800

    format code

--- server/integration-test/src/test/java/com/oceanbase/odc/service/notification/EventFilterTest.java
+++ server/integration-test/src/test/java/com/oceanbase/odc/service/notification/EventFilterTest.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.junit.After;
