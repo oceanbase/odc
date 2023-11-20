21e9eefb8419076ca04e1fe443c5fb72e1eb4e1a
LuckyPickleZZ <lyzhuozz@gmail.com>
2023-11-20 20:28:02 +0800

    remove unused code

--- server/odc-service/src/main/java/com/oceanbase/odc/service/flow/task/BaseODCFlowTaskDelegate.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/flow/task/BaseODCFlowTaskDelegate.java
@@ -276,28 +276,7 @@ public abstract class BaseODCFlowTaskDelegate<T> extends BaseRuntimeFlowableDele
     /**
      * The callback method when the task is successful, used to update the status and other operations
      */
-    protected void onSuccessful(Long taskId, TaskService taskService) {
-        // if (notificationProperties.isEnabled()) {
-        // new Thread(() -> {
-        // TaskEntity taskEntity = taskService.detail(taskId);
-        // ConnectionConfig connection =
-        // connectionService.getWithoutPermissionCheck(taskEntity.getConnectionId());
-        // EventLabels labels = EventUtils.buildEventLabels(taskEntity.getTaskType(), "succeed",
-        // taskEntity.getConnectionId());
-        // Map<String, String> extend = new HashMap<>();
-        // extend.put(EventLabelKeys.VARIABLE_KEY_CLUSTER_NAME, connection.getClusterName());
-        // extend.put(EventLabelKeys.VARIABLE_KEY_TENANT_NAME, connection.getTenantName());
-        // labels.addLabels(extend);
-        // broker.enqueueEvent(Event.builder()
-        // .status(EventStatus.CREATED)
-        // .creatorId(taskEntity.getCreatorId())
-        // .organizationId(taskEntity.getOrganizationId())
-        // .triggerTime(new Date(System.currentTimeMillis()))
-        // .labels(labels)
-        // .build());
-        // }).start();
-        // }
-    }
+    protected void onSuccessful(Long taskId, TaskService taskService) {}
 
     /**
      * The callback method of the task execution timeout, which is used to update the status and other
