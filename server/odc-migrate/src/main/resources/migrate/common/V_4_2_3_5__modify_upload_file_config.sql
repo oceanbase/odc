4e59a86271bc97a9a69586ee60bcf07f246e1ae9
gaoda.xy <x_yang@126.com>
2023-11-21 20:00:43 +0800

    refine

--- server/odc-migrate/src/main/resources/migrate/common/V_4_2_3_5__modify_upload_file_config.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_2_3_5__modify_upload_file_config.sql
@@ -9,17 +9,11 @@ SET
 WHERE
   `key` = 'odc.flow.async.max-upload-file-count';
 
-DELETE FROM
+UPDATE
   `config_system_configuration`
+SET
+  `key` = 'odc.flow.async.max-upload-file-total-size',
+  `value` = '100',
+  `description` = '异步任务最大上传文件数量，默认 100 个'
 WHERE
   `key` = 'odc.flow.async.max-upload-file-total-size}';
-
-INSERT INTO
-  config_system_configuration (`key`, `value`, `description`)
-VALUES(
-    'odc.flow.async.max-upload-file-total-size',
-    '#{32*1024*1024}',
-    '异步任务最大上传文件总大小，单位为字节，默认 32 MB'
-  ) ON DUPLICATE KEY
-UPDATE
-  `value` = '#{32*1024*1024}';
