39ad812f71aaf5eebc264e60932fdc745b20200f
gaoda.xy <x_yang@126.com>
2023-11-22 14:00:38 +0800

    refine sql

--- server/odc-migrate/src/main/resources/migrate/common/V_4_2_3_5__modify_upload_file_config.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_2_3_5__modify_upload_file_config.sql
@@ -1,10 +1,7 @@
 --
 -- Modify upload file config
 --
-UPDATE
+DELETE FROM
   `config_system_configuration`
-SET
-  `value` = '100',
-  `description` = 'Maximum number of uploaded files for database change task, default 100'
 WHERE
-  `key` = 'odc.flow.async.max-upload-file-count';
+  `key` = 'odc.flow.async.max-upload-file-total-size}';
--- server/odc-server/src/main/resources/data.sql
+++ server/odc-server/src/main/resources/data.sql
@@ -332,7 +332,7 @@ VALUES ('odc.flow.async.max-upload-file-count', '500', 'odc', 'default', 'master
 DUPLICATE KEY UPDATE `id`=`id`;
 
 INSERT INTO `config_system_configuration` (`key`, `value`, `application`, `profile`, `label`, `description`)
-VALUES ('odc.flow.async.max-upload-file-total-size}', '#{256*1024*1024}', 'odc', 'default', 'master', '异步任务最大上传文件总大小，单位为字节，默认 256 MB') ON
+VALUES ('odc.flow.async.max-upload-file-total-size', '#{256*1024*1024}', 'odc', 'default', 'master', '异步任务最大上传文件总大小，单位为字节，默认 256 MB') ON
 DUPLICATE KEY UPDATE `id`=`id`;
 
 INSERT INTO config_system_configuration(`key`, `value`, `description`) VALUES('odc.task.file-expire-hours', '336', '流程任务附带文件最多保留小时数，默认 336 小时，即 2 星期') ON DUPLICATE KEY UPDATE `id`=`id`;
@@ -705,5 +705,3 @@ INSERT INTO config_system_configuration ( `key`, `value`, `description` ) VALUES
 
 INSERT INTO config_system_configuration ( `key`, `value`, `description` ) VALUES( 'odc.server.obclient.command-black-list',
  'connect,conn,edit,nopager,notee,pager,print,prompt,rehash,system,tee,resetconnection', 'Prohibited commands that can be executed by obclient' ) ON DUPLICATE KEY UPDATE `id` = `id`;
-
-INSERT INTO config_system_configuration ( `key`, `value`, `description` ) VALUES( 'odc.flow.async.max-upload-file-total-size', '#{32*1024*1024}', 'The maximum total size of uploaded files for database change tasks, in bytes, default 32 MB' ) ON DUPLICATE KEY UPDATE `id` = `id`;
