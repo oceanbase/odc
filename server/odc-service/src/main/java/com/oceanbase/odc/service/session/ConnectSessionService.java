01afffd6bbbab9bfa908c2eb7a164b16fe4cb5a9
MarkPotato777 <yinuo.pyn@oceanbase.com>
2023-12-12 14:48:34 +0800

    optimize imports

--- server/odc-service/src/main/java/com/oceanbase/odc/service/session/ConnectSessionService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/session/ConnectSessionService.java
@@ -37,7 +37,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-import com.oceanbase.odc.common.util.*;
 import com.oceanbase.odc.common.util.SystemUtils;
 import com.oceanbase.odc.core.authority.SecurityManager;
 import com.oceanbase.odc.core.authority.exception.AccessDeniedException;
