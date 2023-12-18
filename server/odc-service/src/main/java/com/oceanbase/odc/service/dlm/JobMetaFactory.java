875d3ab035dcb7575a610493f8abd63e94c41b44
tinker <guowenlong.gwl@oceanbase.com>
2023-12-18 10:21:30 +0800

    merge 4.2.x to current branch.

--- server/odc-service/src/main/java/com/oceanbase/odc/service/dlm/JobMetaFactory.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/dlm/JobMetaFactory.java
@@ -65,16 +65,12 @@ public class JobMetaFactory extends AbstractJobMetaFactory {
         logicTableConfig.setWriterTaskCount(singleTaskThreadPoolSize - logicTableConfig.getReaderTaskCount());
         DataSourceInfo sourceInfo = DataSourceInfoBuilder.build(parameters.getSourceDs());
         DataSourceInfo targetInfo = DataSourceInfoBuilder.build(parameters.getTargetDs());
-        sourceInfo.setConnectionCount(logicTableConfig.getReaderTaskCount()
-                + parameters.getLogicTableConfig().getWriterTaskCount());
-        targetInfo.setConnectionCount(logicTableConfig.getReaderTaskCount()
-                + parameters.getLogicTableConfig().getWriterTaskCount());
-        sourceInfo.setQueryTimeout(taskConnectionQueryTimeout);
-        targetInfo.setQueryTimeout(taskConnectionQueryTimeout);
         sourceInfo.setConnectionCount(2 * (logicTableConfig.getReaderTaskCount()
                 + parameters.getLogicTableConfig().getWriterTaskCount()));
         targetInfo.setConnectionCount(2 * (logicTableConfig.getReaderTaskCount()
                 + parameters.getLogicTableConfig().getWriterTaskCount()));
+        sourceInfo.setQueryTimeout(taskConnectionQueryTimeout);
+        targetInfo.setQueryTimeout(taskConnectionQueryTimeout);
         log.info("Begin to create dlm job,params={}", logicTableConfig);
         // ClusterMeta and TenantMeta used to calculate min limit size.
         JobReq req =
--- server/odc-service/src/main/java/com/oceanbase/odc/service/schedule/ScheduleService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/schedule/ScheduleService.java
@@ -275,7 +275,7 @@ public class ScheduleService {
                 req.setType(JobType.valueOf(taskEntity.getJobGroup()));
                 TriggerConfig triggerConfig = new TriggerConfig();
                 triggerConfig.setTriggerStrategy(TriggerStrategy.START_NOW);
-                quartzJobService.createJob(req,ScheduleTaskUtils.buildTriggerDataMap(taskId));
+                quartzJobService.createJob(req, ScheduleTaskUtils.buildTriggerDataMap(taskId));
                 log.info("Job recreated,jobKey={}", jobKey);
                 return ScheduleTaskResp.withId(taskId);
             } else {
