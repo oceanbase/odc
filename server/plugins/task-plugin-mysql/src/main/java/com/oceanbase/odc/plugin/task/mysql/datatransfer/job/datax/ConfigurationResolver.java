1165080d06fdced0d3d0a96988de85a8adea8be9
LuckyPickleZZ <lyzhuozz@gmail.com>
2023-12-22 15:51:09 +0800

    sql format always requires header

--- server/plugins/task-plugin-mysql/src/main/java/com/oceanbase/odc/plugin/task/mysql/datatransfer/job/datax/ConfigurationResolver.java
+++ server/plugins/task-plugin-mysql/src/main/java/com/oceanbase/odc/plugin/task/mysql/datatransfer/job/datax/ConfigurationResolver.java
@@ -165,7 +165,8 @@ public class ConfigurationResolver {
             pluginParameter.setCsvWriterConfig(getDataXCsvConfig(baseConfig));
             pluginParameter.setSkipHeader(baseConfig.getCsvConfig().isSkipHeader());
             pluginParameter.setNullFormat(baseConfig.getCsvConfig().isBlankToNull() ? "null" : "");
-            if (baseConfig.getCsvConfig().isSkipHeader()) {
+            if (baseConfig.getCsvConfig().isSkipHeader()
+                    && baseConfig.getDataTransferFormat() != DataTransferFormat.SQL) {
                 pluginParameter.setHeader(null);
             }
         }
