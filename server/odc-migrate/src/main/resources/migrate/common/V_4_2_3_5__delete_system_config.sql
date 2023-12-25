a301bf78901c5801aed2c191b43999da192824b0
yh263208 <yh263208@oceanbase.com>
2023-12-25 15:40:05 +0800

    delete unused properties

--- server/odc-migrate/src/main/resources/migrate/common/V_4_2_3_5__delete_system_config.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_2_3_5__delete_system_config.sql
@@ -5,3 +5,8 @@ DELETE FROM
   `config_system_configuration`
 WHERE
   `key` = 'odc.flow.async.max-upload-file-total-size}';
+
+DELETE FROM
+  `config_system_configuration`
+WHERE
+  `key` = 'odc.task.mock-data.max-row-count';
