c48ee60b68a5a204a1d20e492b7a677116ad71ef
krihy <mengdezhicai@gmail.com>
2024-01-05 20:41:00 +0800

    record manualSwapTableTasksChanged

--- server/odc-service/src/main/java/com/oceanbase/odc/service/onlineschemachange/OnlineSchemaChangeFlowableTask.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/onlineschemachange/OnlineSchemaChangeFlowableTask.java
@@ -16,8 +16,10 @@
 package com.oceanbase.odc.service.onlineschemachange;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -86,7 +88,8 @@ public class OnlineSchemaChangeFlowableTask extends BaseODCFlowTaskDelegate<Void
     private volatile long creatorId;
     private volatile long organizationId;
     private volatile boolean continueOnError;
-    private volatile double percentage;
+    private volatile double    percentage;
+    private volatile Set<Long> manualSwapTableStartedScheduleTasks = new HashSet<>();
 
     @Override
     protected Void start(Long taskId, TaskService taskService, DelegateExecution execution) throws Exception {
@@ -165,7 +168,23 @@ public class OnlineSchemaChangeFlowableTask extends BaseODCFlowTaskDelegate<Void
 
             TaskEntity flowTask = taskService.detail(taskId);
             TaskStatus dbStatus = flowTask.getStatus();
-            if (currentPercentage > this.percentage || dbStatus != this.status) {
+
+            Set<Long> currentManualSwapTableTasks  = new HashSet<>();
+            for (ScheduleTaskEntity task : tasks) {
+                OnlineSchemaChangeScheduleTaskResult result = JsonUtils.fromJson(task.getResultJson(),
+                    OnlineSchemaChangeScheduleTaskResult.class);
+                if(result.isManualSwapTableStarted()){
+                    currentManualSwapTableTasks.add(task.getId());
+                }
+            }
+
+            boolean manualSwapTableTasksChanged
+                = !CollectionUtils.isEqualCollection(currentManualSwapTableTasks, manualSwapTableStartedScheduleTasks);
+            if (manualSwapTableTasksChanged) {
+                manualSwapTableStartedScheduleTasks = currentManualSwapTableTasks;
+            }
+
+            if (currentPercentage > this.percentage || dbStatus != this.status || manualSwapTableTasksChanged ) {
                 flowTask.setResultJson(JsonUtils.toJson(new OnlineSchemaChangeTaskResult(tasks.getContent())));
                 flowTask.setStatus(this.status);
                 flowTask.setProgressPercentage(Math.min(currentPercentage, 100));
