e44b6851aeeab2e500d1c4f090e13c36fda6f6dc
gaoda.xy <x_yang@126.com>
2024-01-09 11:32:37 +0800

    fix typo

--- server/odc-migrate/src/main/resources/migrate/common/V_4_2_4_1__alter_iam_permission.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_2_4_1__alter_iam_permission.sql
@@ -1,5 +1,5 @@
 --
--- Add column `expire_time`, `source_type` and `ticket_id` to `iam_permission` table
+-- Add column `expire_time`, `authorization_type` and `ticket_id` to `iam_permission` table
 --
 ALTER TABLE `iam_permission` ADD COLUMN `expire_time` DATETIME DEFAULT NULL COMMENT 'Expiration time of the permission, the default value is null which means it will never expire';
 ALTER TABLE `iam_permission` ADD COLUMN `authorization_type` VARCHAR ( 32 ) NOT NULL DEFAULT 'USER_AUTHORIZATION' COMMENT 'Type of authorization, optional value: USER_AUTHORIZATION ｜ TICKET_APPLICATION';
