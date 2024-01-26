663ff4f8b7e45d9cb34f51a75d8a3d81bdbc2d8f
LuckyPickleZZ <liuyizhuo.lyz@gmail.com>
2024-01-26 14:47:45 +0800

    format code

--- server/integration-test/src/test/java/com/oceanbase/odc/service/datatransfer/DataTransferServiceTest.java
+++ server/integration-test/src/test/java/com/oceanbase/odc/service/datatransfer/DataTransferServiceTest.java
@@ -25,7 +25,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -41,7 +40,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
