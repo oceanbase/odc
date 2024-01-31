d3dbc97c4dce60a3522aae6614a55b719977b309
krihy <mengdezhicai@gmail.com>
2024-01-31 14:45:37 +0800

    format code

--- server/odc-service/src/main/java/com/oceanbase/odc/service/flow/task/BaseODCFlowTaskDelegate.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/flow/task/BaseODCFlowTaskDelegate.java
@@ -212,8 +212,7 @@ public abstract class BaseODCFlowTaskDelegate<T> extends BaseRuntimeFlowableDele
 
     @Override
     protected Callable<T> initCallable(DelegateExecution execution) {
-        return () ->
-            start(taskId, taskService, execution);
+        return () -> start(taskId, taskService, execution);
     }
 
     abstract protected T start(Long taskId, TaskService taskService, DelegateExecution execution) throws Exception;
