60bc9c3d0d6b24f359707a0f4bb2624ee41694f4
IL MARE <yh263208@oceanbase.com>
2024-02-02 12:11:20 +0800

    feat(partition-plan): add implementation for oracle mode (#1485)
    * add service layer and plugin impl
    
    * refactor plugin api
    
    * response to cr comments
    
    * remove useless comments
    
    * imple partition key type recognize
    
    * throw exception
    
    * add datatype factory impl
    
    * rename
    
    * add cell data processor
    
    * add time incre generator
    
    * move partitionplanvarKey
    
    * add sql expr generator
    
    * impl extension point
    
    * add log printing
    
    * rename method
    
    * add partition name gen
    
    * add drop generator
    
    * change extension point method
    
    * add create parti ddl imple
    
    * refact method api
    
    * refact method
    
    * add generate method
    
    * add drop ddl generator imple
    
    * remove existing partition
    
    * add test cases for service layer
    
    * rename class
    
    * refact code
    
    * add parameter for generator
    
    * add interrval settings
    
    * rename parameter
    
    * add interval settings for sql expr gen
    
    * refact the api
    
    * add real name in time data type
    
    * add i18n
    
    * add oracle impl
    
    * add oracle sql gen
    
    * fix unit test error
    
    * add parti name gen for oracle
    
    * remove reload indexes from mysql impl
    
    * add test cases for oracle mode
    
    * fix unit test
    
    * format code
    
    * refact unit test cases
    
    * rename
    
    * add an string utils
    
    * use oracle sql literal
    
    * refact method
    
    * resp to cr comments
    
    * resp to cr comments

--- server/odc-service/src/main/java/com/oceanbase/odc/service/partitionplan/PartitionPlanServiceV2.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/partitionplan/PartitionPlanServiceV2.java
@@ -68,26 +68,35 @@ public class PartitionPlanServiceV2 {
     public Map<PartitionPlanStrategy, List<String>> generatePartitionDdl(@NonNull Connection connection,
             @NonNull DialectType dialectType, @NonNull String schema,
             @NonNull PartitionPlanTableConfig tableConfig) throws Exception {
+        TableExtensionPoint tableExtensionPoint = SchemaPluginUtil.getTableExtension(dialectType);
+        if (tableExtensionPoint == null) {
+            throw new UnsupportedOperationException("Unsupported dialect " + dialectType);
+        }
+        DBTable dbTable = tableExtensionPoint.getDetail(connection, schema, tableConfig.getTableName());
+        return generatePartitionDdl(connection, dialectType, dbTable, tableConfig);
+    }
+
+    public Map<PartitionPlanStrategy, List<String>> generatePartitionDdl(@NonNull Connection connection,
+            @NonNull DialectType dialectType, @NonNull DBTable dbTable,
+            @NonNull PartitionPlanTableConfig tableConfig) throws Exception {
         AutoPartitionExtensionPoint autoPartitionExtensionPoint = TaskPluginUtil
                 .getAutoPartitionExtensionPoint(dialectType);
-        TableExtensionPoint tableExtensionPoint = SchemaPluginUtil.getTableExtension(dialectType);
-        if (tableExtensionPoint == null || autoPartitionExtensionPoint == null) {
+        if (autoPartitionExtensionPoint == null) {
             throw new UnsupportedOperationException("Unsupported dialect " + dialectType);
         }
         String tableName = tableConfig.getTableName();
-        DBTable dbTable = tableExtensionPoint.getDetail(connection, schema, tableName);
         List<DBTableColumn> columns = dbTable.getColumns();
         if (CollectionUtils.isEmpty(columns)) {
             throw new NotFoundException(ResourceType.OB_TABLE, "tableName", tableName);
         }
         DBTablePartition partition = dbTable.getPartition();
         if (partition == null || partition.getPartitionOption() == null) {
-            throw new IllegalArgumentException("Partition is null for " + schema + "." + tableName);
+            throw new IllegalArgumentException("Partition is null for " + tableName);
         } else {
             DBTablePartitionType partitionType = partition.getPartitionOption().getType();
             if (partitionType == DBTablePartitionType.UNKNOWN
                     || partitionType == DBTablePartitionType.NOT_PARTITIONED) {
-                throw new IllegalArgumentException("Table " + schema + "." + tableName + " is not partitioned");
+                throw new IllegalArgumentException("Table " + tableName + " is not partitioned");
             } else if (!autoPartitionExtensionPoint.supports(partition)) {
                 throw new IllegalArgumentException("Unsupported partition type, " + partitionType);
             }
@@ -192,7 +201,7 @@ public class PartitionPlanServiceV2 {
                 throw new IllegalStateException(e);
             }
             return definition;
-        }).filter(d -> removeExistingPartitionElement(dbTable, d)).collect(Collectors.toList()));
+        }).filter(d -> removeExistingPartitionElement(dbTable, d, extensionPoint)).collect(Collectors.toList()));
         strategyListMap.put(PartitionPlanStrategy.DROP, droppedPartitions);
         DBTablePartition partition = dbTable.getPartition();
         return strategyListMap.entrySet().stream().filter(e -> CollectionUtils.isNotEmpty(e.getValue()))
@@ -206,9 +215,11 @@ public class PartitionPlanServiceV2 {
                 }));
     }
 
-    private boolean removeExistingPartitionElement(DBTable dbTable, DBTablePartitionDefinition definition) {
+    private boolean removeExistingPartitionElement(DBTable dbTable, DBTablePartitionDefinition definition,
+            AutoPartitionExtensionPoint extensionPoint) {
         return dbTable.getPartition().getPartitionDefinitions().stream().noneMatch(i -> {
-            if (Objects.equals(i.getName(), definition.getName())) {
+            if (Objects.equals(extensionPoint.unquoteIdentifier(i.getName()),
+                    extensionPoint.unquoteIdentifier(definition.getName()))) {
                 return true;
             }
             int size = Math.min(i.getMaxValues().size(), definition.getMaxValues().size());
---
+++ server/plugins/task-plugin-ob-oracle/src/test/java/com/oceanbase/odc/plugin/task/oboracle/partitionplan/OBOracleTimeIncreasePartitionExprGeneratorTest.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2023 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oceanbase.odc.plugin.task.oboracle.partitionplan;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.text.StringSubstitutor;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.oceanbase.odc.plugin.schema.oboracle.OBOracleTableExtension;
+import com.oceanbase.odc.plugin.task.api.partitionplan.datatype.TimeDataType;
+import com.oceanbase.odc.plugin.task.api.partitionplan.invoker.AutoPartitionKeyInvoker;
+import com.oceanbase.odc.plugin.task.api.partitionplan.invoker.create.PartitionExprGenerator;
+import com.oceanbase.odc.plugin.task.api.partitionplan.model.TimeIncreaseGeneratorConfig;
+import com.oceanbase.odc.plugin.task.oboracle.partitionplan.invoker.create.OBOracleTimeIncreasePartitionExprGenerator;
+import com.oceanbase.odc.test.database.TestDBConfiguration;
+import com.oceanbase.odc.test.database.TestDBConfigurations;
+import com.oceanbase.tools.dbbrowser.model.DBTable;
+
+/**
+ * Test cases for {@link OBOracleTimeIncreasePartitionExprGenerator}
+ *
+ * @author yh263208
+ * @date 2024-01-29 14:51
+ * @since ODC_release_4.2.4
+ */
+public class OBOracleTimeIncreasePartitionExprGeneratorTest {
+
+    public static final String RANGE_TABLE_NAME = "RANGE_PARTI_TIME_TBL";
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        JdbcTemplate oracle = new JdbcTemplate(TestDBConfigurations.getInstance()
+                .getTestOBOracleConfiguration().getDataSource());
+        getDdlContent().forEach(oracle::execute);
+    }
+
+    @AfterClass
+    public static void clear() {
+        JdbcTemplate oracle = new JdbcTemplate(TestDBConfigurations.getInstance()
+                .getTestOBOracleConfiguration().getDataSource());
+        oracle.execute("DROP TABLE " + RANGE_TABLE_NAME);
+    }
+
+    @Test
+    public void generate_secondPrecCurrentMillis_succeed() throws Exception {
+        TestDBConfiguration configuration = TestDBConfigurations.getInstance().getTestOBOracleConfiguration();
+        try (Connection connection = configuration.getDataSource().getConnection()) {
+            OBOracleTableExtension tableExtension = new OBOracleTableExtension();
+            DBTable dbTable = tableExtension.getDetail(connection,
+                    configuration.getDefaultDBName(), RANGE_TABLE_NAME);
+            AutoPartitionKeyInvoker<List<String>> generator = new OBOracleTimeIncreasePartitionExprGenerator();
+            TimeIncreaseGeneratorConfig config = new TimeIncreaseGeneratorConfig();
+            config.setFromCurrentTime(true);
+            config.setInterval(2);
+            config.setIntervalPrecision(TimeDataType.SECOND);
+            List<String> actuals = generator.invoke(connection, dbTable, getParameters(config, 5, "C1"));
+            Assert.assertEquals(5, actuals.size());
+        }
+    }
+
+    @Test
+    public void generate_hourPrecCurrentMillis_succeed() throws Exception {
+        TestDBConfiguration configuration = TestDBConfigurations.getInstance().getTestOBOracleConfiguration();
+        try (Connection connection = configuration.getDataSource().getConnection()) {
+            OBOracleTableExtension tableExtension = new OBOracleTableExtension();
+            DBTable dbTable = tableExtension.getDetail(connection,
+                    configuration.getDefaultDBName(), RANGE_TABLE_NAME);
+            AutoPartitionKeyInvoker<List<String>> generator = new OBOracleTimeIncreasePartitionExprGenerator();
+            TimeIncreaseGeneratorConfig config = new TimeIncreaseGeneratorConfig();
+            long current = System.currentTimeMillis();
+            config.setFromTimestampMillis(current);
+            config.setInterval(5);
+            config.setIntervalPrecision(TimeDataType.HOUR);
+            List<String> actuals = generator.invoke(connection, dbTable, getParameters(config, 5, "C2"));
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date(current));
+            List<String> expects = new ArrayList<>();
+            int i = 0;
+            do {
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH) + 1;
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                expects.add(String.format("Timestamp '%04d-%02d-%02d %02d:00:00'", year, month, day, hour));
+                calendar.add(Calendar.HOUR_OF_DAY, 5);
+            } while ((++i) < 5);
+            Assert.assertEquals(expects, actuals);
+        }
+    }
+
+    @Test
+    public void generate_yearPrecCurrentMillis_succeed() throws Exception {
+        TestDBConfiguration configuration = TestDBConfigurations.getInstance().getTestOBOracleConfiguration();
+        try (Connection connection = configuration.getDataSource().getConnection()) {
+            OBOracleTableExtension tableExtension = new OBOracleTableExtension();
+            DBTable dbTable = tableExtension.getDetail(connection,
+                    configuration.getDefaultDBName(), RANGE_TABLE_NAME);
+            AutoPartitionKeyInvoker<List<String>> generator = new OBOracleTimeIncreasePartitionExprGenerator();
+            TimeIncreaseGeneratorConfig config = new TimeIncreaseGeneratorConfig();
+            long current = System.currentTimeMillis();
+            config.setFromTimestampMillis(current);
+            config.setInterval(5);
+            config.setIntervalPrecision(TimeDataType.MONTH);
+            List<String> actuals = generator.invoke(connection, dbTable, getParameters(config, 5, "C1"));
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date(current));
+            List<String> expects = new ArrayList<>();
+            int i = 0;
+            do {
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH) + 1;
+                expects.add(String.format(
+                        "TO_DATE(' %04d-%02d-01 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')",
+                        year, month));
+                calendar.add(Calendar.MONTH, 5);
+            } while ((++i) < 5);
+            Assert.assertEquals(expects, actuals);
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void generate_illegalDataType_failed() throws Exception {
+        TestDBConfiguration configuration = TestDBConfigurations.getInstance().getTestOBOracleConfiguration();
+        try (Connection connection = configuration.getDataSource().getConnection()) {
+            OBOracleTableExtension tableExtension = new OBOracleTableExtension();
+            DBTable dbTable = tableExtension.getDetail(connection,
+                    configuration.getDefaultDBName(), RANGE_TABLE_NAME);
+            AutoPartitionKeyInvoker<List<String>> generator = new OBOracleTimeIncreasePartitionExprGenerator();
+            TimeIncreaseGeneratorConfig config = new TimeIncreaseGeneratorConfig();
+            long current = System.currentTimeMillis();
+            config.setFromTimestampMillis(current);
+            config.setInterval(5);
+            config.setIntervalPrecision(TimeDataType.SECOND);
+            generator.invoke(connection, dbTable, getParameters(config, 5, "C5"));
+        }
+    }
+
+    private Map<String, Object> getParameters(TimeIncreaseGeneratorConfig config, Integer count, String partitionKey) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(PartitionExprGenerator.GENERATOR_PARAMETER_KEY, config);
+        parameters.put(PartitionExprGenerator.GENERATE_COUNT_KEY, count);
+        parameters.put(PartitionExprGenerator.GENERATOR_PARTITION_KEY, partitionKey);
+        return parameters;
+    }
+
+    private static List<String> getDdlContent() throws IOException {
+        String delimiter = "\\$\\$\\s*";
+        try (InputStream input = OBOraclePartitionKeyDataTypeFactoryTest.class.getClassLoader()
+                .getResourceAsStream("partitionplan/time_partition_generator_create_table_partition.sql")) {
+            byte[] buffer = new byte[input.available()];
+            IOUtils.readFully(input, buffer);
+            StringSubstitutor substitutor = StringSubstitutor.createInterpolator();
+            return new ArrayList<>(Arrays.asList(substitutor.replace(new String(buffer)).split(delimiter)));
+        }
+    }
+
+}
