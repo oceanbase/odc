57f95f068ba02bc53f94a5d68f0c3d9120988535
yizhou <yizhou.xw@gmail.com>
2024-03-01 10:55:32 +0800

    feat(snippet): more builtin snippets (#1682)
    * feat(snippet): more builtin snippets
    
    * fix wrong snippet body
    
    * fix wrong builtin snippet match logic
    
    * fix wrong builtin snippet match logic
    
    * add collect sql plan monitor for mysql mode
    
    * add collect sql plan monitor for mysql mode
    
    * add collect sql plan monitor for mysql mode
    
    * fix
    
    * fix
    
    * fix
    
    * fix circular reference

--- server/odc-service/src/main/java/com/oceanbase/odc/service/snippet/BuiltinSnippetService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/snippet/BuiltinSnippetService.java
@@ -69,11 +69,12 @@ public class BuiltinSnippetService {
     public List<BuiltinSnippet> listBySession(String sessionId) {
         ConnectionSession connectionSession = sessionService.nullSafeGet(sessionId, true);
         String tenantName = ConnectionSessionUtil.getTenantName(connectionSession);
+        boolean isSysTenant = "sys".equals(tenantName);
         String version = ConnectionSessionUtil.getVersion(connectionSession);
         Set<DialectType> supportsDialectTypes = supportsDialectTypes(connectionSession.getConnectType());
         return listAll().stream()
                 .filter(snippet -> supportsDialectTypes.contains(snippet.getDialectType()))
-                .filter(snippet -> "sys".equals(tenantName) == snippet.getTags().contains("sys"))
+                .filter(snippet -> !snippet.isForSysTenantOnly() || isSysTenant)
                 .filter(snippet -> Objects.isNull(snippet.getMinVersion())
                         || snippet.getMinVersion().compareTo(version) <= 0)
                 .filter(snippet -> Objects.isNull(snippet.getMaxVersion())
