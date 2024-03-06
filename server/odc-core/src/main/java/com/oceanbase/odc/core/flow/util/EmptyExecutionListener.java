cc3c0f947474439abe0125fb04024593758fe970
krihy <mengdezhicai@gmail.com>
2024-03-06 22:22:00 +0800

    remove unused code

--- server/odc-core/src/main/java/com/oceanbase/odc/core/flow/util/EmptyExecutionListener.java
+++ server/odc-core/src/main/java/com/oceanbase/odc/core/flow/util/EmptyExecutionListener.java
@@ -37,8 +37,6 @@ public class EmptyExecutionListener extends BaseExecutionListener {
         if (log.isDebugEnabled()) {
             log.debug("Begin execution, activityId={}, processInstanceId={}", execution.getCurrentActivityId(),
                     execution.getProcessInstanceId());
-            execution.getCurrentFlowableListener().getEvent();
-            execution.getCurrentFlowableListener();
         }
     }
 
