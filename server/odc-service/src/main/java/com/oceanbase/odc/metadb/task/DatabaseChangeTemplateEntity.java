69a8aa88b920dae583f4c1552506447725952342
zijiacj <zijia.cj@digital-engine.com>
2024-03-11 21:27:52 +0800

    complete the controller and dao layers

---
+++ server/odc-service/src/main/java/com/oceanbase/odc/metadb/task/DatabaseChangeTemplateEntity.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2024 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oceanbase.odc.metadb.task;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "task_databasechange_template")
+public class DatabaseChangeTemplateEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "creator_id", nullable = false)
+    private Long creatorId;
+
+    @Column(name = "organization_id", nullable = false)
+    private Long organizationId;
+
+    @Column(name = "database_sequences",nullable = false)
+    private String databaseSequences;
+
+    @Generated(GenerationTime.ALWAYS)
+    @Column(name = "create_time", insertable = false, updatable = false)
+    private Date createTime;
+
+    @Generated(GenerationTime.ALWAYS)
+    @Column(name = "update_time", insertable = false, updatable = false)
+    private Date updateTime;
+
+
+}
