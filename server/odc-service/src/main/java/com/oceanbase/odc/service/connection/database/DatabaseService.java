75619e4f3c557ba8505ec40c140bdc5d970e2664
gaoda.xy <x_yang@126.com>
2024-03-13 18:50:59 +0800

    Revert "fix(database-permission): return authorized permission types for detail database interface (#1843)"
    This reverts commit ae99353e08e77aabb23dbc93985178961292c2fd.

--- server/odc-service/src/main/java/com/oceanbase/odc/service/connection/database/DatabaseService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/connection/database/DatabaseService.java
@@ -183,12 +183,12 @@ public class DatabaseService {
     }
 
     private Database getDatabase(Long id) {
-        DatabaseEntity entity = databaseRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException(ResourceType.ODC_DATABASE, "id", id));
-        if (Objects.nonNull(entity.getProjectId())) {
-            projectPermissionValidator.checkProjectRole(entity.getProjectId(), ResourceRoleName.all());
+        Database database = entityToModel(databaseRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(ResourceType.ODC_DATABASE, "id", id)));
+        if (Objects.nonNull(database.getProject()) && Objects.nonNull(database.getProject().getId())) {
+            projectPermissionValidator.checkProjectRole(database.getProject().getId(), ResourceRoleName.all());
         }
-        return entitiesToModels(new PageImpl<>(Collections.singletonList(entity)), true).getContent().get(0);
+        return database;
     }
 
     @SkipAuthorize("odc internal usage")
