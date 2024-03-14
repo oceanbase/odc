66998919000a2c12ab957b9f1fa994013469fd8c
LuckyPickleZZ <liuyizhuo.lyz@oceanbase.com>
2024-03-14 20:58:10 +0800

    fix failed to upload template with roles

--- server/odc-service/src/main/java/com/oceanbase/odc/service/iam/UserBatchImportPreviewer.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/iam/UserBatchImportPreviewer.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -43,7 +42,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.oceanbase.odc.common.util.StringUtils;
 import com.oceanbase.odc.core.authority.util.Authenticated;
 import com.oceanbase.odc.core.authority.util.PreAuthenticate;
+import com.oceanbase.odc.core.shared.Verify;
 import com.oceanbase.odc.core.shared.constant.ErrorCodes;
+import com.oceanbase.odc.core.shared.exception.VerifyException;
 import com.oceanbase.odc.service.common.util.FileConvertUtils;
 import com.oceanbase.odc.service.iam.auth.AuthenticationFacade;
 import com.oceanbase.odc.service.iam.model.BatchImportUser;
@@ -59,21 +60,14 @@ import lombok.extern.slf4j.Slf4j;
 @Authenticated
 public class UserBatchImportPreviewer {
 
-    private static final Pattern REG_NUMBER = Pattern.compile(".*\\d+.*");
-    private static final Pattern REG_UPPERCASE = Pattern.compile(".*[A-Z]+.*");
-    private static final Pattern REG_LOWERCASE = Pattern.compile(".*[a-z]+.*");
-    private static final Pattern REG_SYMBOL = Pattern.compile(".*[._+@#$%]+.*");
-    private static final Pattern SPACE_PATTERN = Pattern.compile("^(?=\\S).+(?<=[^.\\s])$");
-    public static final int PASSWORD_MAX_LENGTH = 32;
-    public static final int PASSWORD_MIN_LENGTH = 8;
-    public static final int NAME_MAX_LENGTH = 64;
+    private static final Pattern ACCOUNT_NAME_PATTERN = Pattern.compile("^[\\w.+@#$%]+$");
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(
+            "^(?=(?:[^0-9]*[0-9]){2})(?=(?:[^A-Z]*[A-Z]){2})(?=(?:[^a-z]*[a-z]){2})"
+                    + "(?=(?:[^._+@#$%]*[._+@#$%]){2})[\\w._+@#$%]{8,32}$");
+    private static final Pattern SPACE_PATTERN = Pattern.compile("^(?=\\S).{1,64}(?<=[^.\\s])$");
     public static final int ACCOUNTNAME_MAX_LENGTH = 64;
     public static final int ACCOUNTNAME_MIN_LENGTH = 4;
     public static final int DESCRIPTION_MAX_LENGTH = 140;
-    public static final int NUMBER_MIN_NUMBER = 2;
-    public static final int UPPER_MIN_NUMBER = 2;
-    public static final int LOWER_MIN_NUMBER = 2;
-    public static final int SYMBOL_MIN_NUMBER = 2;
     @Autowired
     private RoleService roleService;
     @Autowired
@@ -97,7 +91,13 @@ public class UserBatchImportPreviewer {
         Map<String, Role> roleName2RoleMap = roleService.list(Pageable.unpaged()).getContent().stream()
                 .collect(Collectors.toMap(Role::getName, role -> role, (k1, k2) -> k1));
         for (Map<String, String> map : list) {
-            BatchImportUser batchImportUser = createBatchImportUser(map, accountNames, roleName2RoleMap);
+            BatchImportUser batchImportUser;
+            try {
+                batchImportUser = createBatchImportUser(map, accountNames, roleName2RoleMap);
+            } catch (VerifyException e) {
+                batchImportUser = BatchImportUser.ofFail(e.getLocalizedMessage());
+                batchImportUser.setName(map.get(USER_NAME.getLocalizedMessage()));
+            }
             batchImportUserList.add(batchImportUser);
         }
         return UserPreviewBatchImportResp.ofUserExcel(batchImportUserList);
@@ -120,33 +120,31 @@ public class UserBatchImportPreviewer {
     private BatchImportUser createBatchImportUser(Map<String, String> map, Set<String> accountNames,
             Map<String, Role> roleName2RoleMap) {
         BatchImportUser batchImportUser = new BatchImportUser();
-        String accountName = map.get(USER_ACCOUNTNAME.getLocalizedMessage());
-        if (!checkAccountName(accountName) || accountNames.contains(accountName)) {
-            batchImportUser.setErrorMessage("file content error:" + USER_ACCOUNTNAME.getLocalizedMessage());
-        } else {
-            batchImportUser.setAccountName(accountName);
-        }
+
         String name = map.get(USER_NAME.getLocalizedMessage());
-        if (StringUtils.isEmpty(name) || name.length() > NAME_MAX_LENGTH || !SPACE_PATTERN.matcher(name).matches()) {
-            batchImportUser.setErrorMessage("file content error:" + USER_NAME.getLocalizedMessage());
-        } else {
-            batchImportUser.setName(name);
-        }
+        Verify.verify(SPACE_PATTERN.matcher(name).matches(),
+                "username length should not exceed 64 and not start or end with spaces");
+        batchImportUser.setName(name);
+
+        String accountName = map.get(USER_ACCOUNTNAME.getLocalizedMessage());
+        Verify.verify(!accountNames.contains(accountName), "account name already exists");
+        checkAccountName(accountName);
+        batchImportUser.setAccountName(accountName);
+
         String password = map.get(USER_PASSWORD.getLocalizedMessage());
-        if (StringUtils.isEmpty(password) || !checkPassword(password)) {
-            batchImportUser.setErrorMessage("file content error:" + USER_PASSWORD.getLocalizedMessage());
-        } else {
-            batchImportUser.setPassword(password);
-        }
+        Verify.verify(PASSWORD_PATTERN.matcher(password).matches(),
+                "must be 8 to 32 characters long and include at least 2 digits, 2 uppercase letters, "
+                        + "2 lowercase letters, and 2 special characters from ._+@#$%");
+        batchImportUser.setPassword(password);
+
         String enabled = map.get(USER_ENABLED.getLocalizedMessage());
-        if (!StringUtils.equalsIgnoreCase(enabled, "true") && !StringUtils.equalsIgnoreCase(enabled, "false")) {
-            batchImportUser.setErrorMessage("file content error:" + USER_ENABLED.getLocalizedMessage());
-        } else {
-            batchImportUser.setEnabled(enabled.equalsIgnoreCase("true"));
-        }
+        Verify.verify(StringUtils.equalsIgnoreCase(enabled, "true") || StringUtils.equalsIgnoreCase(enabled, "false"),
+                "enabled must be true or false");
+        batchImportUser.setEnabled(enabled.equalsIgnoreCase("true"));
+
         String roleNameList = map.get(USER_ROLEIDS.getLocalizedMessage());
-        batchImportUser.setRoleIds(Collections.emptyList());
-        batchImportUser.setRoleNames(Collections.emptyList());
+        batchImportUser.setRoleIds(new ArrayList<>());
+        batchImportUser.setRoleNames(new ArrayList<>());
         if (StringUtils.isNotBlank(roleNameList)) {
             Set<String> roleNames = new HashSet<>(Arrays.asList(roleNameList.split(",")));
             for (String roleName : roleNames) {
@@ -156,60 +154,22 @@ public class UserBatchImportPreviewer {
                     batchImportUser.getRoleNames().add(role.getName());
                 }
             }
-            if (batchImportUser.getRoleIds().isEmpty()) {
-                batchImportUser.setErrorMessage("file content error:" + USER_ROLEIDS.getLocalizedMessage());
-            }
+            Verify.notEmpty(batchImportUser.getRoleIds(), "Invalid roles");
         }
+
         String description = map.get(USER_DESCRIPTION.getLocalizedMessage());
-        if (description.length() > DESCRIPTION_MAX_LENGTH) {
-            batchImportUser.setErrorMessage("file content error:" + USER_DESCRIPTION.getLocalizedMessage());
-        } else {
-            batchImportUser.setDescription(description);
-        }
+        Verify.notGreaterThan(description.length(), DESCRIPTION_MAX_LENGTH, "description");
+        batchImportUser.setDescription(description);
+
         return batchImportUser;
     }
 
-    private boolean checkPassword(String password) {
-        int numberNumber = 0;
-        int upperNumber = 0;
-        int lowerNumber = 0;
-        int symbolNumber = 0;
-        if (password.length() < PASSWORD_MIN_LENGTH || password.length() > PASSWORD_MAX_LENGTH
-                || password.contains(" ")) {
-            return false;
-        }
-        for (int i = 0; i < password.length(); i++) {
-            String pwd = password.substring(i, i + 1);
-            if (REG_NUMBER.matcher(pwd).matches())
-                numberNumber++;
-            else if (REG_LOWERCASE.matcher(pwd).matches())
-                upperNumber++;
-            else if (REG_UPPERCASE.matcher(pwd).matches())
-                lowerNumber++;
-            else if (REG_SYMBOL.matcher(pwd).matches())
-                symbolNumber++;
-            else
-                return false;
-        }
-        if (numberNumber < NUMBER_MIN_NUMBER || upperNumber < UPPER_MIN_NUMBER || lowerNumber < LOWER_MIN_NUMBER
-                || symbolNumber < SYMBOL_MIN_NUMBER) {
-            return false;
-        }
-        return true;
+    private void checkAccountName(String accountName) {
+        Verify.notNull(accountName, "account name");
+        Verify.notLessThan(accountName.length(), ACCOUNTNAME_MIN_LENGTH, "account name's length");
+        Verify.notGreaterThan(accountName.length(), ACCOUNTNAME_MAX_LENGTH, "account name's length");
+        Verify.verify(ACCOUNT_NAME_PATTERN.matcher(accountName).matches(),
+                "account name must consist of letters, numbers, and special characters in ._+@#$% only. ");
     }
 
-    private boolean checkAccountName(String accountName) {
-        if (accountName == null || accountName.length() < ACCOUNTNAME_MIN_LENGTH
-                || accountName.length() > ACCOUNTNAME_MAX_LENGTH || accountName.contains(" ")) {
-            return false;
-        }
-        for (int i = 0; i < accountName.length(); i++) {
-            String name = accountName.substring(i, i + 1);
-            if (!REG_NUMBER.matcher(name).matches() && !REG_LOWERCASE.matcher(name).matches()
-                    && !REG_UPPERCASE.matcher(name).matches() && !REG_SYMBOL.matcher(name).matches()) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
