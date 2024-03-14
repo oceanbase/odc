ff77fb25ea9e45d3413239c402842e2a0e0bb673
krihy <mengdezhicai@gmail.com>
2024-03-14 17:26:48 +0800

    modify systemconfig

--- server/odc-migrate/src/main/resources/migrate/common/V_4_2_4_12__update_system_config.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_2_4_12__update_system_config.sql
@@ -1,2 +1,3 @@
-update config_system_configuration set `value`='true' where `key` = 'odc.task-framework.enabled';
-update config_system_configuration set `value`='3' where `key` = 'odc.task-framework.executor-waiting-to-run-threshold-seconds';
+update `config_system_configuration` set `value` = 'true' where `key` = 'odc.task-framework.enabled';
+update `config_system_configuration` set `value` = '3' where `key` = 'odc.task-framework.executor-waiting-to-run-threshold-seconds';
+update `config_system_configuration` set `value` = 'true' where `key` = 'odc.notification.enabled';
--- server/odc-migrate/src/main/resources/migrate/common/V_4_2_4_12__update_task_framework_enabled.sql
+++
@@ -1,2 +0,0 @@
-update config_system_configuration set `value`='true' where `key` = 'odc.task-framework.enabled';
-update config_system_configuration set `value`='3' where `key` = 'odc.task-framework.executor-waiting-to-run-threshold-seconds';
