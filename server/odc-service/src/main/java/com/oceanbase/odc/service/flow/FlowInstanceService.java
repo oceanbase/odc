01cc2c7002b5e331bbef0036a22dcef1e5075c17
MarkPotato777 <yinuo.pyn@oceanbase.com>
2024-03-15 10:54:14 +0800

    Revert "optimize"
    This reverts commit 8bf866ef5cd9ad202fe0abda67b3c1b05f242689.

--- server/odc-service/src/main/java/com/oceanbase/odc/service/flow/FlowInstanceService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/flow/FlowInstanceService.java
@@ -376,8 +376,7 @@ public class FlowInstanceService {
                 .and(FlowInstanceViewSpecs.statusIn(params.getStatuses()))
                 .and(FlowInstanceViewSpecs.createTimeLate(params.getStartTime()))
                 .and(FlowInstanceViewSpecs.createTimeBefore(params.getEndTime()))
-                .and(FlowInstanceViewSpecs.idEquals(targetId))
-                .and(FlowInstanceViewSpecs.groupByIdAndTaskType());
+                .and(FlowInstanceViewSpecs.idEquals(targetId));
         if (params.getType() != null) {
             specification = specification.and(FlowInstanceViewSpecs.taskTypeEquals(params.getType()));
         } else {
