dce623a1155dcd8543c65420b98f2909ce3f2544
krihy <kanghongbo.khb@oceanbase.com>
2024-03-21 21:04:44 +0800

    fix for comment1

--- server/odc-service/src/main/java/com/oceanbase/odc/service/task/schedule/MonitorExecutorStatusRateLimiter.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/task/schedule/MonitorExecutorStatusRateLimiter.java
@@ -40,15 +40,12 @@ public class MonitorExecutorStatusRateLimiter implements StartJobRateLimiter {
     private final Supplier<TaskFrameworkProperties> taskFrameworkProperties;
     private final TaskFrameworkService taskFrameworkService;
     private long runningJobCountLimit;
-    private long totalPhysicMemory;
 
     public MonitorExecutorStatusRateLimiter(Supplier<TaskFrameworkProperties> taskFrameworkProperties,
             TaskFrameworkService taskFrameworkService) {
         this.taskFrameworkProperties = taskFrameworkProperties;
         this.taskFrameworkService = taskFrameworkService;
         if (taskFrameworkProperties.get().getRunMode().isProcess() && SystemUtils.isOnLinux()) {
-            this.totalPhysicMemory =
-                    SystemUtils.getSystemTotalPhysicalMemory().convert(BinarySizeUnit.MB).getSizeDigit();
             this.runningJobCountLimit = calculateRunningJobCountLimit();
         }
     }
@@ -57,18 +54,22 @@ public class MonitorExecutorStatusRateLimiter implements StartJobRateLimiter {
     public boolean tryAcquire() {
         if (taskFrameworkProperties.get().getRunMode().isProcess() && SystemUtils.isOnLinux()) {
             long count = taskFrameworkService.countRunningJobs(TaskRunMode.PROCESS);
-            if (count >= runningJobCountLimit && log.isDebugEnabled()) {
-                log.debug("Amount of executor running jobs exceed limit, wait next schedule, limit={}, runningJobs={}.",
-                        runningJobCountLimit, count);
+            if (count >= runningJobCountLimit) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Amount of executor running jobs exceed limit, wait next schedule,"
+                            + " limit={}, runningJobs={}.", runningJobCountLimit, count);
+                }
                 return false;
             }
             long systemFreeMemory = SystemUtils.getSystemFreePhysicalMemory().convert(BinarySizeUnit.MB).getSizeDigit();
             long processMemoryMinSize = taskFrameworkProperties.get().getJobProcessMinMemorySizeInMB();
             long memoryReserveSize = taskFrameworkProperties.get().getSystemReserveMinFreeMemorySizeInMB();
 
-            if (systemFreeMemory < (memoryReserveSize + processMemoryMinSize) && log.isDebugEnabled()) {
-                log.debug("Free memory lack, systemFreeMemory={}, processMemoryMinSize={}, "
-                        + "memoryReserveSize={}", systemFreeMemory, processMemoryMinSize, memoryReserveSize);
+            if (systemFreeMemory < (memoryReserveSize + processMemoryMinSize)) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Free memory lack, systemFreeMemory={}, processMemoryMinSize={}, "
+                            + "memoryReserveSize={}", systemFreeMemory, processMemoryMinSize, memoryReserveSize);
+                }
                 return false;
             }
             return true;
@@ -78,8 +79,9 @@ public class MonitorExecutorStatusRateLimiter implements StartJobRateLimiter {
     }
 
     private long calculateRunningJobCountLimit() {
-        long minProcessMem = taskFrameworkProperties.get().getJobProcessMinMemorySizeInMB();
+        long totalPhysicMemory = SystemUtils.getSystemTotalPhysicalMemory().convert(BinarySizeUnit.MB).getSizeDigit();
         long jvmXmx = SystemUtils.getJvmXmxMemory().convert(BinarySizeUnit.MB).getSizeDigit();
+        long minProcessMem = taskFrameworkProperties.get().getJobProcessMinMemorySizeInMB();
         // limitCount = (totalPhysicMemory*0.8 - jvmXmx)/jobProcessMinMemorySize
         return new BigDecimal(totalPhysicMemory * 0.8).subtract(BigDecimal.valueOf(jvmXmx))
                 .divide(new BigDecimal(minProcessMem), RoundingMode.FLOOR)
@@ -94,9 +96,11 @@ public class MonitorExecutorStatusRateLimiter implements StartJobRateLimiter {
 
         boolean continued = taskFrameworkProperties.getExecutorWaitingToRunThresholdCount() - count > 0;
         if (!continued && log.isDebugEnabled()) {
-            log.debug("Amount of executors waiting to run exceed threshold, wait next schedule,"
-                    + " threshold={}, waitingJobs={}.",
-                    taskFrameworkProperties.getExecutorWaitingToRunThresholdCount(), count);
+            if (log.isDebugEnabled()) {
+                log.debug("Amount of executors waiting to run exceed threshold, wait next schedule,"
+                        + " threshold={}, waitingJobs={}.",
+                        taskFrameworkProperties.getExecutorWaitingToRunThresholdCount(), count);
+            }
         }
         return continued;
     }
