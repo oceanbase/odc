297d4dc181d6f71465bc2feed9a7fcc88a027220
krihy <kanghongbo.khb@oceanbase.com>
2024-03-22 18:41:19 +0800

    fix reverse log

--- server/integration-test/src/test/java/com/oceanbase/odc/service/task/TestLogUtils.java
+++ server/integration-test/src/test/java/com/oceanbase/odc/service/task/TestLogUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 OceanBase.
+ * Copyright (c) 2023 OceanBase.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.oceanbase.odc.service.task;
 
 import java.io.File;
@@ -33,7 +32,7 @@ import com.oceanbase.odc.service.task.executor.logger.LogUtils;
 public class TestLogUtils {
 
     @Test
-    public void test_readLogContent() {
+    public void test_readPartLogContent() {
         String fileName = "./test.log";
         File file = new File(fileName);
         try (FileOutputStream a = new FileOutputStream(file)) {
@@ -45,6 +44,26 @@ public class TestLogUtils {
             a.write("\n".getBytes());
             String s = LogUtils.getLogContent(fileName, 2L, 100L);
             Assert.assertTrue(s.startsWith("java\ngo\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            file.delete();
+        }
+    }
+
+    @Test
+    public void test_readAllLogContent() {
+        String fileName = "./test2.log";
+        File file = new File(fileName);
+        try (FileOutputStream a = new FileOutputStream(file)) {
+            a.write("test".getBytes());
+            a.write("\n".getBytes());
+            a.write("java".getBytes());
+            a.write("\n".getBytes());
+            a.write("go".getBytes());
+            a.write("\n".getBytes());
+            String s = LogUtils.getLogContent(fileName, 3L, 100L);
+            Assert.assertEquals("test\njava\ngo\n", s);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
