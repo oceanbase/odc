0c16a2ea4a6610ef936b31fa0bc8fd1c2ee36b0a
krihy <kanghongbo.khb@oceanbase.com>
2024-03-25 14:04:51 +0800

    format code

--- server/odc-service/src/main/java/com/oceanbase/odc/service/logger/LoggerService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/logger/LoggerService.java
@@ -92,7 +92,8 @@ public class LoggerService {
                 // check log file is exist on current disk
                 String logFileStr = LogUtils.getTaskLogFileWithPath(jobEntity.getId(), level);
                 if (new File(logFileStr).exists()) {
-                    return LogUtils.getLogContentReversed(logFileStr, LogUtils.MAX_LOG_LINE_COUNT, LogUtils.MAX_LOG_BYTE_COUNT);
+                    return LogUtils.getLogContentReversed(logFileStr, LogUtils.MAX_LOG_LINE_COUNT,
+                            LogUtils.MAX_LOG_BYTE_COUNT);
                 }
 
                 File tempFile = cloudObjectStorageService.downloadToTempFile(objId.get());
@@ -101,7 +102,8 @@ public class LoggerService {
                 } finally {
                     FileUtils.deleteQuietly(tempFile);
                 }
-                return LogUtils.getLogContentReversed(logFileStr, LogUtils.MAX_LOG_LINE_COUNT, LogUtils.MAX_LOG_BYTE_COUNT);
+                return LogUtils.getLogContentReversed(logFileStr, LogUtils.MAX_LOG_LINE_COUNT,
+                        LogUtils.MAX_LOG_BYTE_COUNT);
 
             }
         }
