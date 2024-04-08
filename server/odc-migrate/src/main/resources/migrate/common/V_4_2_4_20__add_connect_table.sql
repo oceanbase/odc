5d45abd8f67f4af4855053b1bdee22edc694d8c8
ISADBA <chinafenghao@hotmail.com>
2024-04-08 15:01:39 +0800

    feat(permission-table): a. 完成数据库变更工单创建，预检查鉴权 b. 完成数据库导出工单创建，预检查鉴权 c. 完成数据库结果集导出工单创建，预检查鉴权 d. 完成表权限相关审计日志功能 e. 完成表权限相关消息通知功能 f. 国际化适配完成 g. 提供一个申请权限使用的无鉴权数据表列表 h. 支持过期表权限清理
    2024-04-08

--- server/odc-migrate/src/main/resources/migrate/common/V_4_2_4_20__add_connect_table.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_2_4_20__add_connect_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `connect_table` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `database_id` bigint(20) NOT NULL comment "relation connect_database's id column",
+  `database_id` bigint(20) NOT NULL comment "relation connect_database id column",
   `name` varchar(256) NOT NULL comment "table name",
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `sync_status` varchar(64) NOT NULL COMMENT 'synchronizing status, enum values: PENDING, SUCCEEDED, FAILED',
+  `sync_status` varchar(64) NOT NULL COMMENT "synchronizing status, enum values: PENDING, SUCCEEDED, FAILED",
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_db_id_tablename` (`database_id`, `name`)
 );
