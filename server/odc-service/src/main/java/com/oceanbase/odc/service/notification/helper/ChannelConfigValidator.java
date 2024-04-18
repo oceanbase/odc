68c2323190f1d76369493f4b7193fa7ee0280d68
LuckyPickleZZ <liuyizhuo.lyz@oceanbase.com>
2024-04-18 13:08:40 +0800

    report if whitelist is empty

--- server/odc-service/src/main/java/com/oceanbase/odc/service/notification/helper/ChannelConfigValidator.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/notification/helper/ChannelConfigValidator.java
@@ -90,7 +90,7 @@ public class ChannelConfigValidator {
         Verify.verify(
                 channelConfig.getWebhook().startsWith("http://") || channelConfig.getWebhook().startsWith("https://"),
                 "Webhook should start with 'http://' or 'https://'");
-        Verify.verify(SSRFChecker.checkUrlInWhiteList(channelConfig.getWebhook(),
+        Verify.verify(SSRFChecker.checkUrlInWhiteListStrictly(channelConfig.getWebhook(),
                 integrationConfigProperties.getUrlWhiteList()),
                 "The webhook is not in white list, please add it into system configuration with the key 'odc.integration.url-white-list'");
 
