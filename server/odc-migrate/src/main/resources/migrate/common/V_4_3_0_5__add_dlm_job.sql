a62d4848e83f3e7f9643aa46e9b918072a9b4be3
tinker <guowenlong.gwl@oceanbase.com>
2024-05-16 10:25:49 +0800

    update sql script

--- server/odc-migrate/src/main/resources/migrate/common/V_4_3_0_5__add_dlm_job.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_3_0_5__add_dlm_job.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `dlm_job` (
+CREATE TABLE IF NOT EXISTS `dlm_job` (
   `id` bigint AUTO_INCREMENT NOT NULL COMMENT 'ID of the dlm job object',
   `schedule_task_id` bigint NOT NULL COMMENT 'ID of the related schedule task, refer to schedule_task.id',
   `dlm_job_id` varchar(120) NOT NULL COMMENT 'Unique identifier of the dlm job object',
@@ -12,12 +12,12 @@ CREATE TABLE `dlm_job` (
   `parameters` text NOT NULL COMMENT 'JSON format of various parameters/settings for the dlm job',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Record insertion time',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Record modification time',
-INDEX `idx_dlm_job_schedule_task_id` USING BTREE (`schedule_task_id`) LOCAL,
+INDEX `idx_dlm_job_schedule_task_id` (`schedule_task_id`),
 CONSTRAINT `pk_dlm_job_id` PRIMARY KEY (`id`),
 CONSTRAINT `uk_dlm_job_dlm_job_id` UNIQUE (`dlm_job_id`)
 );
 
-CREATE TABLE `dlm_job_statistic` (
+CREATE TABLE IF NOT EXISTS `dlm_job_statistic` (
   `id` bigint AUTO_INCREMENT NOT NULL COMMENT 'ID of the dlm job statistic object',
   `dlm_job_id` varchar(120) NOT NULL COMMENT 'The dlm job ID that this statistic is associated with',
   `processed_row_count` bigint NULL COMMENT 'Total number of rows processed by the dlm job',
