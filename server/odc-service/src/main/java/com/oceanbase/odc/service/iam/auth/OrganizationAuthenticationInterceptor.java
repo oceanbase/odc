c33bc3aff2ab05e2338053a807f5850cc8412cf3
zhangxiao <140503120+PeachThinking@users.noreply.github.com>
2024-07-02 11:20:35 +0800

    fix(auth): access denied for current project interface (#2862)

--- server/odc-service/src/main/java/com/oceanbase/odc/service/iam/auth/OrganizationAuthenticationInterceptor.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/iam/auth/OrganizationAuthenticationInterceptor.java
@@ -65,6 +65,8 @@ public class OrganizationAuthenticationInterceptor implements HandlerInterceptor
             "/api/v2/datasource/sessions/*/sqls/*/download",
             "/api/v2/config/**",
             "/api/v2/snippet/builtinSnippets",
+            "/api/v2/cloud/collaboration/currentProject",
+            "/api/v2/snippet/builtinSnippets",
             "/api/v2/datasource/datasources/template",
             "/api/v2/iam/users/template"
     };
