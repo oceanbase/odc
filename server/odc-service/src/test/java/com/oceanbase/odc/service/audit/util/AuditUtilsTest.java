5ade6fadacc1cf9f9cc6d8f87582c43e32b8c4b1
CHLK <keyang.lk@antgroup.com>
2024-07-02 20:32:26 +0800

    style: format AuditUtilsTest

--- server/odc-service/src/test/java/com/oceanbase/odc/service/audit/util/AuditUtilsTest.java
+++ server/odc-service/src/test/java/com/oceanbase/odc/service/audit/util/AuditUtilsTest.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collection;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
