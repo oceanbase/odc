253fe1223cdec91a62cb0cbd2feb6d71c61276d6
MarkPotato777 <yinuo.pyn@oceanbase.com>
2024-07-05 13:39:37 +0800

    fix mvn build rpm

--- script/functions.sh
+++ script/functions.sh
@@ -369,9 +369,9 @@ function maven_build_rpm() {
     pushd "${ODC_DIR}" || return 2
 
     func_echo "maven build rpm package starting..."
-    if ! mvn --file server/odc-server/pom.xml rpm:rpm \
+    if ! mvn " $mvn_extra_args" --file server/odc-server/pom.xml rpm:rpm \
         -Drpm.prefix=${RPM_DEFAULT_INSTALL_PREFIX} \
-        -Drpm.release=${rpm_release} " $mvn_extra_args"; then
+        -Drpm.release=${rpm_release}; then
         func_echo "maven build rpm failed"
         popd
         return 3
