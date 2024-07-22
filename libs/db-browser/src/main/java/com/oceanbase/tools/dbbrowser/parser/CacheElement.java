2fcaec4447b172378e1b6a35c5690da1722efc08
yh263208@oceanbase.com <yh263208@oceanbase.com>
2024-07-22 11:22:23 +0800

    several bug fix

--- libs/db-browser/src/main/java/com/oceanbase/tools/dbbrowser/parser/CacheElement.java
+++ libs/db-browser/src/main/java/com/oceanbase/tools/dbbrowser/parser/CacheElement.java
@@ -15,32 +15,64 @@
  */
 package com.oceanbase.tools.dbbrowser.parser;
 
+import java.util.Objects;
+
 import lombok.NonNull;
 
 class CacheElement<T> {
 
     private final T value;
     private final Exception exception;
+    private final StackTraceElement[] traceElements;
 
     public CacheElement(@NonNull T value) {
         this.exception = null;
         this.value = value;
+        this.traceElements = null;
     }
 
     public CacheElement(@NonNull Exception exception) {
         this.value = null;
         this.exception = exception;
+        this.traceElements = getRealStackTraceElements(exception);
     }
 
-    public T get() {
+    public synchronized T get() {
         if (this.exception != null) {
+            Exception target = setStackTraceElements(this.exception);
             if (this.exception instanceof RuntimeException) {
-                throw (RuntimeException) this.exception;
+                throw (RuntimeException) target;
             } else {
-                throw new RuntimeException(this.exception);
+                throw new RuntimeException(target);
             }
         }
         return this.value;
+    }
+
+    private StackTraceElement[] getRealStackTraceElements(Exception e) {
+        StackTraceElement elt = Thread.currentThread().getStackTrace()[3];
+        int i;
+        for (i = 0; i < e.getStackTrace().length; i++) {
+            StackTraceElement item = e.getStackTrace()[i];
+            if (Objects.equals(item.getClassName(), elt.getClassName())
+                    && Objects.equals(item.getMethodName(), elt.getMethodName())) {
+                i++;
+                break;
+            }
+        }
+        StackTraceElement[] target = new StackTraceElement[i];
+        System.arraycopy(e.getStackTrace(), 0, target, 0, i);
+        return target;
+    }
+
+    private Exception setStackTraceElements(Exception e) {
+        StackTraceElement[] elts = Thread.currentThread().getStackTrace();
+        int length = this.traceElements.length;
+        StackTraceElement[] target = new StackTraceElement[length + elts.length - 4];
+        System.arraycopy(this.traceElements, 0, target, 0, length);
+        System.arraycopy(elts, 4, target, length, elts.length - 4);
+        e.setStackTrace(target);
+        return e;
     }
 
 }
