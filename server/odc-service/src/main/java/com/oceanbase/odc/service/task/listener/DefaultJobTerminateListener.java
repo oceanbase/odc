743c034e8331032598841065563f6c24e4f51382
guowl3 <70270140+guowl3@users.noreply.github.com>
2024-07-25 15:42:05 +0800

    fix(dlm): don't compare the table structure if syncTableStructure is off (#3014)

--- server/odc-service/src/main/java/com/oceanbase/odc/service/task/listener/DefaultJobTerminateListener.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/task/listener/DefaultJobTerminateListener.java
@@ -77,7 +77,7 @@ public class DefaultJobTerminateListener extends AbstractEventListener<JobTermin
                 scheduleService.refreshScheduleStatus(Long.parseLong(o.getJobName()));
                 // Trigger the data-delete job if necessary after the data-archive task is completed.
                 if (parameters.getJobType() == com.oceanbase.tools.migrator.common.enums.JobType.MIGRATE
-                        && parameters.isDeleteAfterMigration()) {
+                        && parameters.isDeleteAfterMigration() && taskStatus == TaskStatus.DONE) {
                     scheduleService.dataArchiveDelete(Long.parseLong(o.getJobName()), o.getId());
                     log.info("Trigger delete job succeed.");
                 }
