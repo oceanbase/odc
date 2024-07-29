96128df9db14b414eb5c8c15deed6e8dfe0dc019
yiminpeng <yiminp@yeah.net>
2024-07-29 16:07:00 +0800

    add component

--- server/odc-core/src/main/java/com/oceanbase/odc/core/migrate/DefaultMigratePreHook.java
+++ server/odc-core/src/main/java/com/oceanbase/odc/core/migrate/DefaultMigratePreHook.java
@@ -17,9 +17,12 @@ package com.oceanbase.odc.core.migrate;
 
 import javax.sql.DataSource;
 
+import org.springframework.stereotype.Component;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class DefaultMigratePreHook implements MigratePreHook {
 
     @Override
