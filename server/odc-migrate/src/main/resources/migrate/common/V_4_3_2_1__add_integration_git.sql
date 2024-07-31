b57524c003aa60185cda3df387ef6fcf13c02166
LuckyPickleZZ <liuyizhuo.lyz@oceanbase.com>
2024-07-31 13:37:28 +0800

    init dao layer for git integration

---
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_3_2_1__add_integration_git.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS `integration_git_repository` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `creator_id` bigint(20) NOT NULL COMMENT 'creator user id, references iam_user.id',
+  `organization_id` bigint(20) NOT NULL COMMENT 'organization id, references iam_organization.id',
+  `project_id` bigint(20) NOT NULL COMMENT 'project id, references collaboration_project.id',
+  `description` varchar(512) DEFAULT NULL COMMENT 'description',
+  `name` varchar(512) NOT NULL COMMENT 'repository name',
+  `provider_type` varchar(32) NOT NULL COMMENT 'git provider type',
+  `provider_url` varchar(128) NOT NULL COMMENT 'git provider url',
+  `clone_url` varchar(512) NOT NULL COMMENT 'https clone url, like https://github.com/xxx/xxx.git'
+  `email` varchar(128) NOT NULL COMMENT 'user email for commit',
+  `token` varchar(512) NOT NULL COMMENT 'person access token',
+  PRIMARY KEY (`id`),
+  KEY `integration_git_repository_project_id` (`project_id`)
+) COMMENT = 'notification channel configs';
+
+CREATE TABLE IF NOT EXISTS `integration_git_repository_stage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `description` varchar(512) DEFAULT NULL COMMENT 'description',
+  `repo_id` bigint(20) NOT NULL COMMENT 'git repository id, references integration_git_repository.id',
+  `state` varchar(32) NOT NULL COMMENT 'local repository state',
+  `branch` varchar(512) NOT NULL COMMENT 'last edit branch',
+  `last_commit_id` varchar(64) DEFAULT NULL COMMENT 'git commit revision number',
+  `diff_patch_storage` varchar(512) DEFAULT NULL COMMENT 'storage information of patch file',
+  `user_id` bigint(20) NOT NULL COMMENT 'user id, references iam_user.id'
+  PRIMARY KEY (`id`),
+  KEY `integration_git_edit_history_repo_id_user_id` (`repo_id`, `user_id`)
+) COMMENT = 'notification channel configs';
---
+++ server/odc-service/src/main/java/com/oceanbase/odc/metadb/integration/git/GitRepositoryEntity.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2024 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oceanbase.odc.metadb.integration.git;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.oceanbase.odc.service.integration.git.model.VcsProvider;
+
+import lombok.Data;
+
+/**
+ * @author: liuyizhuo.lyz
+ * @date: 2024/7/29
+ */
+@Data
+@Entity
+@Table(name = "integration_git_repository")
+public class GitRepositoryEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "create_time", insertable = false, updatable = false)
+    private Date createTime;
+
+    @Column(name = "update_time", insertable = false, updatable = false)
+    private Date updateTime;
+
+    @Column(name = "creator_id", nullable = false)
+    private Long creatorId;
+
+    @Column(name = "organization_id", nullable = false)
+    private Long organizationId;
+
+    @Column(name = "project_id", nullable = false)
+    private Long projectId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider_type", nullable = false)
+    private VcsProvider providerType;
+
+    @Column(name = "provider_url", nullable = false)
+    private String providerUrl;
+
+    @Column(name = "ssh_url", nullable = false)
+    private String sshUrl;
+
+    @Column(name = "clone_url", nullable = false)
+    private String cloneUrl;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "token", nullable = false)
+    private String token;
+
+    @Column(name = "salt", nullable = false)
+    private String salt;
+
+}
