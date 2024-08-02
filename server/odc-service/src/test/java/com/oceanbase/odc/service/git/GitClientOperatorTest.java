fab36c2f5f64e185b32988d940373b17aaa325e3
LuckyPickleZZ <liuyizhuo.lyz@oceanbase.com>
2024-08-02 15:59:47 +0800

    format code

--- server/odc-service/src/test/java/com/oceanbase/odc/service/git/GitClientOperatorTest.java
+++ server/odc-service/src/test/java/com/oceanbase/odc/service/git/GitClientOperatorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 OceanBase.
+ * Copyright (c) 2023 OceanBase.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.oceanbase.odc.service.git.GitClientOperator;
 import com.oceanbase.odc.service.git.model.FileChangeType;
 import com.oceanbase.odc.service.git.model.GitDiff;
 
