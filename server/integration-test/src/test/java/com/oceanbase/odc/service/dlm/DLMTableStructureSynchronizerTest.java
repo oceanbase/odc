cf961877d588d5356f54530ccfad6d97c4c4b4b7
tinker <guowenlong.gwl@oceanbase.com>
2024-08-06 14:00:22 +0800

    code format

--- server/integration-test/src/test/java/com/oceanbase/odc/service/dlm/DLMTableStructureSynchronizerTest.java
+++ server/integration-test/src/test/java/com/oceanbase/odc/service/dlm/DLMTableStructureSynchronizerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 OceanBase.
+ * Copyright (c) 2023 OceanBase.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.oceanbase.odc.service.dlm;
 
 import org.junit.Test;
@@ -30,14 +29,16 @@ import cn.hutool.core.lang.Assert;
 public class DLMTableStructureSynchronizerTest {
 
     @Test
-    public void isSupportedSyncTableStructure_success(){
-        boolean supportedSyncTableStructure = DLMTableStructureSynchronizer.isSupportedSyncTableStructure(DialectType.MYSQL,"5.7.0",DialectType.MYSQL,"5.7.0");
+    public void isSupportedSyncTableStructure_success() {
+        boolean supportedSyncTableStructure = DLMTableStructureSynchronizer
+                .isSupportedSyncTableStructure(DialectType.MYSQL, "5.7.0", DialectType.MYSQL, "5.7.0");
         Assert.isTrue(supportedSyncTableStructure);
     }
 
     @Test
-    public void isSupportedSyncTableStructure_failed(){
-        boolean supportedSyncTableStructure = DLMTableStructureSynchronizer.isSupportedSyncTableStructure(DialectType.MYSQL,"5.6.0",DialectType.OB_MYSQL,"5.7.0");
+    public void isSupportedSyncTableStructure_failed() {
+        boolean supportedSyncTableStructure = DLMTableStructureSynchronizer
+                .isSupportedSyncTableStructure(DialectType.MYSQL, "5.6.0", DialectType.MYSQL, "5.7.0");
         Assert.isFalse(supportedSyncTableStructure);
     }
 }
