9467e6d4bbce154bfa2af9b795ba73d7b259972e
CHLK <30882682+CHLK@users.noreply.github.com>
2024-08-13 20:11:20 +0800

    feat(worksheet): add controller define for project files (#3089)
    * feat(projectfiles): add controller define for project files
    
    * style(projectfiles): format BatchUploadProjectFileReq
    
    * style(projectfiles): format license
    
    * feat(projectfiles): update the definition of batchUploadFiles and batchDeleteFiles in ProjectFilesController
    
    * fix(projectfiles):can't find symbol
    
    * refactor(worksheet):rename module name from projectfiles to worksheet, use english language in code comment, rename name in WorksheetLocation to value, change generateTempCredential to generateUploadUrl, use interface WorksheetServiceFacade and impl with WorksheetServiceFacadeImpl
    
    * refactor(worksheet):rename WorksheetType\UploadWorksheetTuple
    
    * style(projectfiles): format WorksheetMetaResp
    
    * refactor(worksheet): change the request url of WorksheetController, optimize the request and response of /worksheets/generateUploadUrl,and other little change
    
    * refactor(worksheet): change the request url of WorksheetController, optimize the request and response of /worksheets/generateUploadUrl,and other little change
    
    * refactor(worksheet): add @Data
    
    * refactor(worksheet): rename destination to destinationPath
    
    * refactor(worksheet): format code
    
    * refactor(worksheet): rename objectKey to objectId,consistent with the naming in the project
    
    * feat(worksheet): change the field of GenerateWorksheetUploadUrlReq
    
    * feat(worksheet): change GenerateWorksheetUploadUrlResp
    
    * feat(worksheet): change field of GenerateWorksheetUploadUrlResp
    
    * refactor(worksheet): merge search API to list,and standardize interface definition of WorksheetController
    
    * refactor(worksheet): remove search api from  WorksheetController

---
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/worksheet/model/GenerateWorksheetUploadUrlResp.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oceanbase.odc.service.worksheet.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * the request of generating worksheet upload url
+ *
+ * @author keyang
+ * @date 2024/08/08
+ * @since 4.3.2
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenerateWorksheetUploadUrlResp {
+    /**
+     * the upload url to upload the file to object storage
+     */
+    String uploadUrl;
+    /**
+     * the object key in cloud storage,to locate uploaded files
+     */
+    String objectId;
+}
