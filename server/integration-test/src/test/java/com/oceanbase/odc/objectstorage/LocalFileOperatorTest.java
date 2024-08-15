87d9bd316f77157d86ffd61eaf2da73f54cdc4fe
liuke <keyang.lk@antgroup.com>
2024-08-15 21:08:40 +0800

    feat(objectstorage):format code

--- server/integration-test/src/test/java/com/oceanbase/odc/objectstorage/LocalFileOperatorTest.java
+++ server/integration-test/src/test/java/com/oceanbase/odc/objectstorage/LocalFileOperatorTest.java
@@ -28,7 +28,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import org.mockito.InjectMocks;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
