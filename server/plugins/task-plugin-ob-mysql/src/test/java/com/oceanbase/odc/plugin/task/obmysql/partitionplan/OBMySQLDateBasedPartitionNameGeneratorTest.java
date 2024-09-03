6eefdb640819a147734c32a1124faab3a2c7469d
tinker <guowenlong.gwl@oceanbase.com>
2024-09-03 10:15:49 +0800

    fix ut

--- server/plugins/task-plugin-ob-mysql/src/test/java/com/oceanbase/odc/plugin/task/obmysql/partitionplan/OBMySQLDateBasedPartitionNameGeneratorTest.java
+++ server/plugins/task-plugin-ob-mysql/src/test/java/com/oceanbase/odc/plugin/task/obmysql/partitionplan/OBMySQLDateBasedPartitionNameGeneratorTest.java
@@ -61,7 +61,7 @@ public class OBMySQLDateBasedPartitionNameGeneratorTest {
             config.setRefPartitionKey("col");
             PartitionNameGenerator generator = new OBMySQLDateBasedPartitionNameGenerator();
             String actual = generator.invoke(connection, dbTable, getParameters(config));
-            String expect = "p20240229";
+            String expect = "p20240301";
             Assert.assertEquals(expect, actual);
         }
     }
@@ -73,6 +73,7 @@ public class OBMySQLDateBasedPartitionNameGeneratorTest {
         parameters.put(PartitionNameGenerator.TARGET_PARTITION_DEF_KEY, definition);
         parameters.put(PartitionNameGenerator.TARGET_PARTITION_DEF_INDEX_KEY, 0);
         parameters.put(PartitionNameGenerator.PARTITION_NAME_GENERATOR_KEY, config);
+        parameters.put(PartitionNameGenerator.PREVIOUS_PARTITION_EXPRS,Collections.singletonList("'2024-03-01 00:00:00'"));
         return parameters;
     }
 
