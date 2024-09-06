e89549a482edc8864b06a2a1942b94d86511f3a3
IL MARE <yh263208@oceanbase.com>
2024-09-06 17:19:58 +0800

    refactor(dfa): move it from odc-core to odc-common (#3299)

--- server/odc-common/src/main/java/com/oceanbase/odc/common/dfa/AbstractDfa.java
+++ server/odc-common/src/main/java/com/oceanbase/odc/common/dfa/AbstractDfa.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.oceanbase.odc.core.dfa;
+package com.oceanbase.odc.common.dfa;
 
 import java.util.List;
 import java.util.stream.Collectors;
