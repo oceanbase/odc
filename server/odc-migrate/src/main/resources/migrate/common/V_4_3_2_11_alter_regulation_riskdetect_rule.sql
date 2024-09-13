794a3618b9379dd8fd88fb25fa1ffcfdfac935f7
yiminpeng <yiminp@yeah.net>
2024-09-13 17:14:54 +0800

    optimize

---
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_3_2_11_alter_regulation_riskdetect_rule.sql
@@ -0,0 +1,4 @@
+--
+-- Alter column `value_json` size from varchar(2048) to varchar(10240) in `regulation_riskdetect_rule` table
+--
+alter table `regulation_riskdetect_rule` modify column `value_json` mediumtext default null COMMENT 'json of rule value';
