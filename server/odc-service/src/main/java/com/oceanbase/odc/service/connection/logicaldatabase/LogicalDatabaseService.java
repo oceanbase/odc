15b0042a02388dfb2db8b00f6bb9561c9683db7d
pynzzZ <yinuo.pyn@oceanbase.com>
2024-09-14 12:37:24 +0800

    fix(logicaldatabase): logical database last sync time not shown (#3408)

--- server/odc-service/src/main/java/com/oceanbase/odc/service/connection/logicaldatabase/LogicalDatabaseService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/connection/logicaldatabase/LogicalDatabaseService.java
@@ -18,6 +18,7 @@ package com.oceanbase.odc.service.connection.logicaldatabase;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -148,6 +149,8 @@ public class LogicalDatabaseService {
         logicalDatabase.setConnectType(baseConnection.getType());
         logicalDatabase.setSyncStatus(DatabaseSyncStatus.INITIALIZED);
         logicalDatabase.setObjectSyncStatus(DBObjectSyncStatus.INITIALIZED);
+        logicalDatabase.setLastSyncTime(new Date());
+        logicalDatabase.setObjectLastSyncTime(new Date());
         logicalDatabase.setExisted(true);
         logicalDatabase.setOrganizationId(organizationId);
         DatabaseEntity savedLogicalDatabase = databaseRepository.saveAndFlush(logicalDatabase);
