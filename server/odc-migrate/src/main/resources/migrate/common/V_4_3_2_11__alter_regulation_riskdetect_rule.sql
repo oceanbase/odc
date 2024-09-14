dd065d202b405821716f668b50c5c3f7887db204
yiminpeng <yiminp@yeah.net>
2024-09-14 15:15:01 +0800

    update olddata 2 new column

--- server/odc-migrate/src/main/resources/migrate/common/V_4_3_2_11__alter_regulation_riskdetect_rule.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_3_2_11__alter_regulation_riskdetect_rule.sql
@@ -1,5 +1,9 @@
 --
--- Alter column `value_json` size from varchar(2048) to MEDIUMTEXT in `regulation_riskdetect_rule` table
+--  v4.3.2.11
 --
+-- alter column `value_json` size from varchar(2048) to MEDIUMTEXT
 ALTER TABLE `regulation_riskdetect_rule` CHANGE COLUMN `value_json` `value_json_old` VARCHAR(2048);
 ALTER TABLE `regulation_riskdetect_rule` ADD COLUMN `value_json` MEDIUMTEXT DEFAULT NULL;
+
+-- update `value_json` column from `value_json_old`
+UPDATE `regulation_riskdetect_rule` SET `value_json` = `value_json_old`;
