16b31ed96e5f0b6e197387f236e970925a0db883
yiminpeng <yiminp@yeah.net>
2024-09-14 09:22:48 +0800

    add default value

--- server/odc-migrate/src/main/resources/migrate/common/V_4_3_2_11_alter_regulation_riskdetect_rule.sql
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_3_2_11_alter_regulation_riskdetect_rule.sql
@@ -2,4 +2,4 @@
 -- Alter column `value_json` size from varchar(2048) to MEDIUMTEXT in `regulation_riskdetect_rule` table
 --
 ALTER TABLE `regulation_riskdetect_rule` CHANGE COLUMN `value_json` `value_json_old` VARCHAR(2048);
-ALTER TABLE `regulation_riskdetect_rule` ADD COLUMN `value_json` MEDIUMTEXT;
+ALTER TABLE `regulation_riskdetect_rule` ADD COLUMN `value_json` MEDIUMTEXT DEFAULT NULL;
