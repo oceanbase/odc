a5b4c06ad970248f2214fddefb3a9efbd28d02ed
yh263208@oceanbase.com <yh263208@oceanbase.com>
2024-09-26 21:37:21 +0800

    add vector db ds

---
+++ server/odc-service/src/main/java/com/oceanbase/odc/config/VectorDBConfiguration.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2023 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oceanbase.odc.config;
+
+import java.util.Objects;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.sql.DataSource;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.oceanbase.odc.common.concurrent.ExecutorUtils;
+import com.oceanbase.odc.common.util.SystemUtils;
+import com.oceanbase.odc.config.jpa.EnhancedJpaRepository;
+import com.oceanbase.odc.core.task.TaskThreadFactory;
+import com.oceanbase.odc.service.session.SessionProperties;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Configuration
+@MapperScan(basePackages = {"com.oceanbase.odc.vectordb"})
+@EnableJpaRepositories(
+        basePackages = {"com.oceanbase.odc.vectordb"},
+        repositoryBaseClass = EnhancedJpaRepository.class,
+        transactionManagerRef = "vectordbTransactionManager",
+        entityManagerFactoryRef = "vectordbEntityManagerFactory")
+@EntityScan({"com.oceanbase.odc.vectordb"})
+@EnableTransactionManagement
+@ConditionalOnProperty(prefix = "odc.datasource.vectordb", name = {"url", "driver-class-name", "username", "password"})
+public class VectorDBConfiguration {
+
+    @Value("${odc.datasource.vectordb.username}")
+    private String username;
+    @Value("${odc.datasource.vectordb.password}")
+    private String password;
+    @Value("${odc.datasource.vectordb.url}")
+    private String url;
+    @Value("${odc.datasource.vectordb.driver-class-name}")
+    private String driverClassName;
+    @Autowired
+    private SessionProperties sessionProperties;
+    private ThreadPoolExecutor vectorDbBootstrapExecutor;
+
+    @PostConstruct
+    public void init() {
+        int poolSize = Math.max(SystemUtils.availableProcessors(), 5);
+        this.vectorDbBootstrapExecutor = new ThreadPoolExecutor(poolSize, poolSize, 0,
+                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(),
+                new TaskThreadFactory("vectordb-bootstrap-executor"), new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+
+    @PreDestroy
+    public void destroy() {
+        ExecutorUtils.gracefulShutdown(vectorDbBootstrapExecutor, "vectordb-bootstrap-executor", 5);
+    }
+
+    @Bean(name = "vectordbDataSource")
+    public DataSource vectordbDataSource() {
+        return DataSourceBuilder.create()
+                .url(this.url)
+                .username(this.username)
+                .password(this.password)
+                .driverClassName(this.driverClassName)
+                .build();
+    }
+
+    @Bean(name = "vectordbEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean vectordbEntityManagerFactory(
+            EntityManagerFactoryBuilder builder, @Qualifier("vectordbDataSource") DataSource dataSource) {
+        builder.setBootstrapExecutor(new ConcurrentTaskExecutor(vectorDbBootstrapExecutor));
+        return builder.dataSource(dataSource).packages("com.oceanbase.odc.vectordb").build();
+    }
+
+    @Bean(name = "vectordbTransactionManager")
+    public PlatformTransactionManager vectordbTransactionManager(
+            @Qualifier("vectordbEntityManagerFactory") LocalContainerEntityManagerFactoryBean vectordbEntityManagerFactory) {
+        return new JpaTransactionManager(Objects.requireNonNull(vectordbEntityManagerFactory.getObject()));
+    }
+
+}
