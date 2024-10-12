76e209185bc61975d544fa245c3c2ec84e712aaf
jingtian <jingtian.zx@oceanbase.com>
2024-10-12 16:53:34 +0800

    modify QueueCapacity of syncDatabaseTaskExecutor

--- server/odc-service/src/main/java/com/oceanbase/odc/config/ScheduleConfiguration.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/config/ScheduleConfiguration.java
@@ -190,12 +190,12 @@ public class ScheduleConfiguration {
         int poolSize = Math.max(SystemUtils.availableProcessors() * 8, 64);
         executor.setCorePoolSize(poolSize);
         executor.setMaxPoolSize(poolSize);
-        executor.setQueueCapacity(0);
+        executor.setQueueCapacity(Integer.MAX_VALUE);
         executor.setThreadNamePrefix("database-sync-");
         executor.setWaitForTasksToCompleteOnShutdown(true);
         executor.setAwaitTerminationSeconds(5);
         executor.setTaskDecorator(new TraceDecorator<>());
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
         executor.initialize();
         log.info("syncDatabaseTaskExecutor initialized");
         return executor;
