970a18903641ae17bfa47b19c393bbc308450804
ungreat <luyiang.lya@alibaba-inc.com>
2025-03-03 17:52:16 +0800

    add cancel flow without permission

--- server/odc-service/src/main/java/com/oceanbase/odc/service/flow/FlowInstanceService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/flow/FlowInstanceService.java
@@ -547,9 +547,15 @@ public class FlowInstanceService {
     }
 
     @Transactional(rollbackFor = Exception.class)
-    public FlowInstanceDetailResp cancel(@NotNull Long id, Boolean skipAuth) {
+    public FlowInstanceDetailResp cancelWithWritePermission(@NotNull Long id, Boolean skipApproveAuth) {
         FlowInstance flowInstance = mapFlowInstanceWithWritePermission(id, flowInst -> flowInst);
-        return cancel(flowInstance, skipAuth);
+        return cancel(flowInstance, skipApproveAuth);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public FlowInstanceDetailResp cancelWithoutPermission(@NotNull Long id, Boolean skipApproveAuth) {
+        FlowInstance flowInstance = mapFlowInstanceWithoutPermissionCheck(id, flowInst -> flowInst);
+        return cancel(flowInstance, skipApproveAuth);
     }
 
     public Map<Long, FlowStatus> getStatus(Set<Long> ids) {
