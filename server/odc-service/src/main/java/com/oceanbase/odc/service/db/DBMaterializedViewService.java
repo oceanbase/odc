51c61295036b8dc5eb8c2c278322ba300a17b267
zijia.cj <zijia.cj@digital-engine.com>
2025-03-07 14:50:10 +0800

    Add listAllBases method and enhance imports in DBMaterializedViewService

--- server/odc-service/src/main/java/com/oceanbase/odc/service/db/DBMaterializedViewService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/db/DBMaterializedViewService.java
@@ -17,13 +17,17 @@ package com.oceanbase.odc.service.db;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotNull;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.ConnectionCallback;
 import org.springframework.stereotype.Service;
@@ -32,6 +36,8 @@ import com.oceanbase.odc.core.authority.util.SkipAuthorize;
 import com.oceanbase.odc.core.session.ConnectionSession;
 import com.oceanbase.odc.core.session.ConnectionSessionConstants;
 import com.oceanbase.odc.core.session.ConnectionSessionUtil;
+import com.oceanbase.odc.core.shared.constant.ConnectType;
+import com.oceanbase.odc.core.shared.constant.OdcConstants;
 import com.oceanbase.odc.metadb.dbobject.DBObjectRepository;
 import com.oceanbase.odc.plugin.schema.api.MVExtensionPoint;
 import com.oceanbase.odc.service.connection.database.DatabaseService;
@@ -40,7 +46,11 @@ import com.oceanbase.odc.service.connection.model.ConnectionConfig;
 import com.oceanbase.odc.service.connection.table.TableService;
 import com.oceanbase.odc.service.connection.table.model.QueryTableParams;
 import com.oceanbase.odc.service.connection.table.model.Table;
+import com.oceanbase.odc.service.db.browser.DBSchemaAccessors;
+import com.oceanbase.odc.service.db.model.AllTablesAndViews;
 import com.oceanbase.odc.service.db.model.DBViewResponse;
+import com.oceanbase.odc.service.db.model.DatabaseAndTables;
+import com.oceanbase.odc.service.db.model.DatabaseAndViews;
 import com.oceanbase.odc.service.db.model.MVSyncDataReq;
 import com.oceanbase.odc.service.iam.auth.AuthenticationFacade;
 import com.oceanbase.odc.service.plugin.SchemaPluginUtil;
@@ -49,6 +59,7 @@ import com.oceanbase.tools.dbbrowser.model.DBMVSyncDataParameter;
 import com.oceanbase.tools.dbbrowser.model.DBObjectIdentity;
 import com.oceanbase.tools.dbbrowser.model.DBObjectType;
 import com.oceanbase.tools.dbbrowser.model.DBView;
+import com.oceanbase.tools.dbbrowser.schema.DBSchemaAccessor;
 
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -93,6 +104,49 @@ public class DBMaterializedViewService {
         tableService.generateListAndSyncDBTablesByTableType(params, database, connectionConfig, tables, null,
                 DBObjectType.MATERIALIZED_VIEW, latestTableNames);
         return tables;
+    }
+
+    public AllTablesAndViews listAllBases(ConnectionSession connectionSession,
+        String tableNameLike) {
+        AllTablesAndViews allResult = new AllTablesAndViews();
+        DBSchemaAccessor accessor = DBSchemaAccessors.create(connectionSession);
+        List<DatabaseAndTables> tables = new ArrayList<>();
+        if (connectionSession.getConnectType().equals(ConnectType.ODP_SHARDING_OB_MYSQL)) {
+            List<String> names = accessor.showTablesLike(null, tableNameLike).stream()
+                .filter(name -> !StringUtils.endsWithIgnoreCase(name, OdcConstants.VALIDATE_DDL_TABLE_POSTFIX))
+                .collect(Collectors.toList());
+            DatabaseAndTables databaseAndTables = new DatabaseAndTables(
+                ConnectionSessionUtil.getCurrentSchema(connectionSession), names);
+            tables.add(databaseAndTables);
+        } else {
+            List<String> databases = accessor.showDatabases();
+            tables = databases.stream().map(schema -> {
+                    List<String> tablesLike = accessor.showTablesLike(schema, tableNameLike).stream()
+                        .filter(name -> !StringUtils.endsWith(name.toUpperCase(),
+                            OdcConstants.VALIDATE_DDL_TABLE_POSTFIX))
+                        .collect(Collectors.toList());
+                    return tablesLike.size() != 0 ? new DatabaseAndTables(schema, tablesLike)
+                        : new DatabaseAndTables();
+                }).filter(item -> item.getDatabaseName() != null)
+                .sorted(Comparator.comparing(DatabaseAndTables::getDatabaseName)).collect(Collectors.toList());
+        }
+
+        List<DBObjectIdentity> viewsIdentities = accessor.listAllViews(tableNameLike);
+        Map<String, List<String>> schema2views = new HashMap<>();
+        viewsIdentities.forEach(item -> {
+            List<String> views = schema2views.computeIfAbsent(item.getSchemaName(), t -> new ArrayList<>());
+            views.add(item.getName());
+        });
+        List<DatabaseAndViews> views = new ArrayList<>();
+        schema2views.forEach((schema, viewNames) -> {
+            DatabaseAndViews view = new DatabaseAndViews();
+            view.setDatabaseName(schema);
+            view.setViews(viewNames);
+            views.add(view);
+        });
+        allResult.setTables(tables);
+        allResult.setViews(views);
+        return allResult;
     }
 
     public DBViewResponse detail(ConnectionSession connectionSession, String schemaName, String viewName) {
