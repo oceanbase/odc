1de2c5d84e15decbc28de25f64848133367a478f
LuckyPickleZZ <liuyizhuo.lyz@oceanbase.com>
2025-03-10 14:02:56 +0800

    fix UT failed

--- server/plugins/task-plugin-api/src/main/java/com/oceanbase/odc/plugin/task/api/datatransfer/dumper/BinaryFile.java
+++ server/plugins/task-plugin-api/src/main/java/com/oceanbase/odc/plugin/task/api/datatransfer/dumper/BinaryFile.java
@@ -16,7 +16,6 @@
 package com.oceanbase.odc.plugin.task.api.datatransfer.dumper;
 
 import java.io.InputStream;
-import java.lang.reflect.Method;
 import java.net.URL;
 
 import com.oceanbase.tools.loaddump.utils.SerializeUtils;
