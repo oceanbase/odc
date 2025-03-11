a7d8ff47d0b7c541eca3239fd043084b9243631a
LuckyPickleZZ <liuyizhuo.lyz@oceanbase.com>
2025-03-11 10:35:15 +0800

    fix UT

--- server/integration-test/src/test/java/com/oceanbase/odc/service/datatransfer/ExportOutputTest.java
+++ server/integration-test/src/test/java/com/oceanbase/odc/service/datatransfer/ExportOutputTest.java
@@ -28,7 +28,6 @@ import com.oceanbase.odc.plugin.task.api.datatransfer.dumper.AbstractOutputFile;
 import com.oceanbase.odc.plugin.task.api.datatransfer.dumper.DumpDBObject;
 import com.oceanbase.odc.plugin.task.api.datatransfer.dumper.ExportOutput;
 import com.oceanbase.tools.loaddump.common.enums.ObjectType;
-import com.oceanbase.tools.loaddump.utils.SerializeUtils;
 
 /**
  * Test cases for {@link ExportOutput}
@@ -40,14 +39,12 @@ import com.oceanbase.tools.loaddump.utils.SerializeUtils;
 public class ExportOutputTest {
 
     @Test
-    //@Ignore
     public void getManifest_fromZipFile_returnNotNull() throws IOException {
         ExportOutput output = new ExportOutput(getDumpZip());
         Assert.assertNotNull(output.getManifest());
     }
 
     @Test
-    //@Ignore
     public void getManifest_fromFolder_returnNotNull() throws Exception {
         ExportOutput output = new ExportOutput(getDumpFolder());
         Assert.assertNotNull(output.getManifest());
