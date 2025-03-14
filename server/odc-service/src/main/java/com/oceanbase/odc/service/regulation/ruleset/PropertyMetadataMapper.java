7a0037d07cda825a3a3fe9db62aaf6d6f5302862
yiminpeng <yiminp@yeah.net>
2025-03-14 11:11:16 +0800

    add max query limit config

--- server/odc-service/src/main/java/com/oceanbase/odc/service/regulation/ruleset/PropertyMetadataMapper.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/regulation/ruleset/PropertyMetadataMapper.java
@@ -24,6 +24,8 @@ import org.apache.commons.collections4.CollectionUtils;
 
 import com.oceanbase.odc.core.shared.Verify;
 import com.oceanbase.odc.metadb.regulation.ruleset.PropertyMetadataEntity;
+import com.oceanbase.odc.service.common.util.SpringContextUtil;
+import com.oceanbase.odc.service.config.OrganizationConfigFacade;
 import com.oceanbase.odc.service.regulation.ruleset.model.PropertyMetadata;
 import com.oceanbase.odc.service.regulation.ruleset.model.PropertyType;
 
@@ -59,7 +61,14 @@ public class PropertyMetadataMapper {
                 model.setDefaultValue(Collections.emptyList());
             } else {
                 Verify.equals(1, entity.getDefaultValues().size(), "defaultValues.size");
-                model.setDefaultValue(Integer.parseInt(entity.getDefaultValues().get(0)));
+                Integer defaultValue = Integer.valueOf(entity.getDefaultValues().get(0));
+                if (entity.getName().contains("sql-console.max-return-rows")) {
+                    model.setDefaultValue(
+                        SpringContextUtil.getBean(OrganizationConfigFacade.class)
+                            .getDefaultMaxQueryLimit(defaultValue));
+                } else {
+                    model.setDefaultValue(defaultValue);
+                }
             }
             if (CollectionUtils.isNotEmpty(candidates)) {
                 model.setCandidates(candidates.stream().map(Integer::parseInt).collect(Collectors.toList()));
