85f7ba817f99c70e7a77525219c08ffb778de52f
liuke <keyang.lk@antgroup.com>
2025-03-17 16:28:57 +0800

    style(Script): format code

--- server/integration-test/src/test/java/com/oceanbase/odc/service/script/ScriptServiceWithLocalTest.java
+++ server/integration-test/src/test/java/com/oceanbase/odc/service/script/ScriptServiceWithLocalTest.java
@@ -28,7 +28,6 @@ import java.io.InputStreamReader;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
