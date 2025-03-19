8446fc11c28cc450f30e8d46dff3b07cfdf777a6
ungreat <luyiang.lya@alibaba-inc.com>
2025-03-19 23:46:40 +0800

    support web schedule export

--- server/odc-service/src/main/java/com/oceanbase/odc/service/schedule/export/ScheduleExportService.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/schedule/export/ScheduleExportService.java
@@ -18,6 +18,9 @@ package com.oceanbase.odc.service.schedule.export;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,16 +29,31 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.oceanbase.odc.common.security.PasswordUtils;
+import com.oceanbase.odc.core.shared.OrganizationIsolated;
 import com.oceanbase.odc.core.shared.PreConditions;
+import com.oceanbase.odc.core.shared.constant.OrganizationType;
+import com.oceanbase.odc.core.shared.constant.ResourceRoleName;
+import com.oceanbase.odc.core.shared.constant.ResourceType;
+import com.oceanbase.odc.metadb.flow.FlowInstanceEntity;
+import com.oceanbase.odc.metadb.schedule.ScheduleRepository;
 import com.oceanbase.odc.service.common.util.OdcFileUtil;
 import com.oceanbase.odc.service.exporter.model.ExportProperties;
 import com.oceanbase.odc.service.exporter.model.ExportedFile;
+import com.oceanbase.odc.service.flow.FlowInstanceService;
+import com.oceanbase.odc.service.iam.HorizontalDataPermissionValidator;
+import com.oceanbase.odc.service.iam.ProjectPermissionValidator;
 import com.oceanbase.odc.service.iam.auth.AuthenticationFacade;
 import com.oceanbase.odc.service.objectstorage.ObjectStorageFacade;
 import com.oceanbase.odc.service.objectstorage.model.ObjectMetadata;
+import com.oceanbase.odc.service.schedule.ScheduleService;
 import com.oceanbase.odc.service.schedule.export.model.FileExportResponse;
 import com.oceanbase.odc.service.schedule.export.model.ScheduleTaskExportRequest;
+import com.oceanbase.odc.service.schedule.model.Schedule;
+import com.oceanbase.odc.service.schedule.model.ScheduleMapper;
 import com.oceanbase.odc.service.schedule.model.ScheduleType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Service
 public class ScheduleExportService {
@@ -51,12 +69,29 @@ public class ScheduleExportService {
     @Autowired
     private AuthenticationFacade authenticationFacade;
 
+    @Autowired
+    private ProjectPermissionValidator projectPermissionValidator;
+
+    @Autowired
+    private FlowInstanceService flowInstanceService;
+
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @Autowired
+    private HorizontalDataPermissionValidator horizontalDataPermissionValidator;
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
     private static String getPersonalBucketName(String userIdStr) {
         PreConditions.notEmpty(userIdStr, "userIdStr");
         return ASYNC_TASK_BASE_BUCKET.concat(File.separator).concat(userIdStr);
     }
 
     public FileExportResponse export(ScheduleTaskExportRequest request) {
+        checkRequestIdsPermission(request);
+
         ExportProperties properties = scheduleTaskExporter.generateExportProperties();
         String encryptKey = new BCryptPasswordEncoder().encode(PasswordUtils.random());
 
@@ -70,6 +105,30 @@ public class ScheduleExportService {
         }
         return mapToFileExportResponse(exportedFile);
     }
+
+
+    private void checkRequestIdsPermission(ScheduleTaskExportRequest request) {
+        Set<Long> projectIds;
+        if (request.getScheduleType().equals(ScheduleType.PARTITION_PLAN)) {
+            List<FlowInstanceEntity> flowInstanceEntities = flowInstanceService.listByIds(request.getIds());
+            projectIds =
+                    flowInstanceEntities.stream().map(FlowInstanceEntity::getProjectId).collect(Collectors.toSet());
+            List<FlowOrganizationIsolated> flowOrganizationIsolateds = flowInstanceEntities.stream().map(
+                    f -> new FlowOrganizationIsolated(f.getOrganizationId(), f.getId())).collect(
+                            Collectors.toList());
+            horizontalDataPermissionValidator.checkCurrentOrganization(flowOrganizationIsolateds);
+        } else {
+            List<Schedule> scheduleEntities = scheduleRepository.findByIdIn(request.getIds()).stream()
+                    .map(ScheduleMapper.INSTANCE::entityToModel).collect(
+                            Collectors.toList());
+            horizontalDataPermissionValidator.checkCurrentOrganization(scheduleEntities);
+            projectIds = scheduleEntities.stream().map(Schedule::getProjectId).collect(Collectors.toSet());
+        }
+        if (authenticationFacade.currentOrganization().getType().equals(OrganizationType.TEAM)) {
+            projectPermissionValidator.checkProjectRole(projectIds, ResourceRoleName.all());
+        }
+    }
+
 
     private FileExportResponse mapToFileExportResponse(ExportedFile exportedFile) {
         FileExportResponse fileExportResponse = new FileExportResponse();
@@ -91,4 +150,26 @@ public class ScheduleExportService {
         return fileExportResponse;
     }
 
+    @Data
+    @AllArgsConstructor
+    private final static class FlowOrganizationIsolated implements OrganizationIsolated {
+
+        private Long organizationId;
+        private Long id;
+
+        @Override
+        public String resourceType() {
+            return ResourceType.ODC_FLOW_INSTANCE.name();
+        }
+
+        @Override
+        public Long organizationId() {
+            return organizationId;
+        }
+
+        @Override
+        public Long id() {
+            return id;
+        }
+    }
 }
