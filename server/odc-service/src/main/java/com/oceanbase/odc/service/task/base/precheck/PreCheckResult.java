18bfa7eef3fad3d6cc9b78a4c0831c99af444bb3
ysjemmm <mayang.ysj@digital-engine.com>
2025-03-19 19:02:47 +0800

    add code

---
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/task/base/precheck/PreCheckResult.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oceanbase.odc.service.task.base.precheck;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.oceanbase.odc.service.sqlcheck.model.CheckViolation;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @Author: ysj
+ * @Date: 2025/3/15 18:16
+ * @Since: 4.3.4
+ * @Description:
+ */
+@Data
+@Accessors(chain = true)
+public class PreCheckResult {
+    private long affectedRows;
+    private List<CheckViolation> checkViolations;
+
+    public static PreCheckResult empty() {
+        return new PreCheckResult()
+                .setAffectedRows(0L)
+                .setCheckViolations(Collections.emptyList());
+    }
+
+}
