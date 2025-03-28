21c603990b107076f0868a56f3b19d2e582a6030
zijia.cj <zijia.cj@digital-engine.com>
2025-03-28 18:21:40 +0800

    feat(materialized view): materialized view adapt to existing functions in odc
    * adapt sql console feature for Materialized View
    
    * Remove init editable info comment
    
    * Refactor test code to use mView instead of dbMaterializedView
    
    * Add support for create/drop materialized view for ob mysql and ob oracle
    
    * Remove duplicated constants in VersionDiffConfigService
    
    * Fix sql parser for drop view and mview
    
    * Add test cases for create/drop materialized view and view
    
    * Fix bug: Add materialized view to table permission check
    
    * Refactor SqlExecuteResult to support external table and materialized view
    
    * automatic completion for materialized view
    
    * modify code format
    
    * Optimize sql execute result editable info

--- server/odc-service/src/main/java/com/oceanbase/odc/service/permission/table/ApplyTablePermissionPreprocessor.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/permission/table/ApplyTablePermissionPreprocessor.java
@@ -117,7 +117,8 @@ public class ApplyTablePermissionPreprocessor implements Preprocessor {
             table.setDatabaseId(database.getId());
             table.setDatabaseName(database.getName());
             if (table.getType() == DBObjectType.TABLE || table.getType() == DBObjectType.VIEW
-                    || table.getType() == DBObjectType.EXTERNAL_TABLE) {
+                    || table.getType() == DBObjectType.EXTERNAL_TABLE
+                    || table.getType() == DBObjectType.MATERIALIZED_VIEW) {
                 ConnectionConfig dataSource = id2DataSource.get(database.getDataSource().getId());
                 if (dataSource == null) {
                     throw new NotFoundException(ResourceType.ODC_CONNECTION, "id", database.getDataSource().getId());
