f4b84aa6b5c81f04a0dfc07cf47c4edb6a498ee6
yiminpeng <yiminp@yeah.net>
2025-04-08 21:50:40 +0800

    add db operation

--- server/odc-service/src/main/java/com/oceanbase/odc/metadb/config/OrganizationConfigDAO.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/metadb/config/OrganizationConfigDAO.java
@@ -45,6 +45,22 @@ public class OrganizationConfigDAO {
     }
 
     /**
+     * Query the organization configuration entity based on the organizationId and key
+     *
+     * @param organizationId organization ID
+     * @param key configuration key
+     * @return config entity
+     */
+    public OrganizationConfigEntity queryByOrganizationIdAndKey(Long organizationId, String key) {
+        PreConditions.notNull(organizationId, "organizationId");
+        PreConditions.notBlank(key, "key");
+        String sql = "SELECT organization_id, `key`, `value`, create_time, update_time, description"
+                + " FROM config_organization_configuration WHERE organization_id = ? AND `key` = ?";
+        return jdbcTemplate.queryForObject(sql,
+                new BeanPropertyRowMapper<>(OrganizationConfigEntity.class), organizationId, key);
+    }
+
+    /**
      * Batch insert or update organization configuration entity list
      *
      * @param entities config list
