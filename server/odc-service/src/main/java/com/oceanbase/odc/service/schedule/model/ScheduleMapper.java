31c8fa3905c729ae31f8dc1ad1634b85f5a3e3d6
ungreat <luyiang.lya@alibaba-inc.com>
2025-04-14 14:57:29 +0800

    fix

--- server/odc-service/src/main/java/com/oceanbase/odc/service/schedule/model/ScheduleMapper.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/schedule/model/ScheduleMapper.java
@@ -48,6 +48,8 @@ public interface ScheduleMapper {
 
     ScheduleEntity modelToEntity(Schedule model);
 
+    @Mapping(target = "scheduleStatus",source = "entity.status")
+    @Mapping(target = "scheduleStatus",source = "entity.type")
     ScheduleExportListView toScheduleExportListView(ScheduleEntity entity);
 
     @Named("entityToParameters")
