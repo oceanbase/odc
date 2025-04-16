86b4529986de4a0e2b8b835ac1624377d0906fb3
zijia.cj <zijia.cj@digital-engine.com>
2025-04-16 13:00:58 +0800

    Remove unused import in test class

--- libs/db-browser/src/test/java/com/oceanbase/tools/dbbrowser/schema/OBMySQLSchemaAccessorTest.java
+++ libs/db-browser/src/test/java/com/oceanbase/tools/dbbrowser/schema/OBMySQLSchemaAccessorTest.java
@@ -19,7 +19,6 @@ import static com.oceanbase.tools.dbbrowser.editor.DBObjectUtilsTest.loadAsStrin
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
