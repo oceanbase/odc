84b558ccff0d48602968db1092d8cb191ca29d4c
梓烟 <ziyan.fzy@digital-engine.com>
2025-05-29 15:34:59 +0800

    PullRequest: 6 feat(aci):add aci pipeline function
    Merge branch ziyan_aci_pipeline_configuration of git@code.alipay.com:oceanbase-cloud-platform/odc-mirror.git into main
    https://code.alipay.com/oceanbase-cloud-platform/odc-mirror/pull_requests/6
    
    Reviewed-by: 天客 <guowenlong.gwl@oceanbase.com>
    
    
    * update aci
    * update aci
    * update aci
    * update aci
    * update aci
    * update aci
    * update aci
    * update aci
    * update aci
    * update aci
    * update
    * update aci
    * update aci
    * update aci
    * update aci
    * update aci
    * update aci
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * test aci
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * aci 测试
    * 实现推送 jar 包到 oss
    * 实现推送 jar 包到 oss
    * feat:添加doris有效数据源
    * 实现推送 jar 包到 oss
    * 实现推送 jar 包到 oss
    * 测试部分流水线节点
    * 测试部分流水线节点
    * 测试部分流水线节点
    * 测试部分流水线节点
    * 测试部分流水线节点
    * 更新oss配置，bucket为 odc-build
    * 更新oss配置，bucket为 odc-build
    * 整理流水线代码
    * 执行流水线测试
    * 支持上传依赖文件
    * 支持上传依赖文件
    * 支持上传依赖文件
    * 设置PMD扫描可跳过
    * test
    * fix:本地依赖安装到指定路径下
    * fix:本地依赖安装到指定路径下
    * fix:本地依赖安装到指定路径下
    * 构建rpm
    * 优化function脚本中nodejs升级至16的逻辑
    * resp comments.
    * resp comments.
    * resp comments.
    * resp comments.
    * resp comments.
    * resp comments.
    * resp comments.
    * resp comments.
    * resp comments.
    * resp comments.
    * resp comments.
    * 升级单元测试组件至2.0以支持设置环境变量
    * 升级单元测试组件至2.0以支持设置环境变量
    * 升级单元测试组件至2.0以支持设置环境变量
    * 升级单元测试组件至2.0以支持设置环境变量
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * mvn指定仓库地址测试
    * 前端打包测试
    * 前端打包测试
    * 前端打包测试
    * 前端打包测试
    * 前端打包测试
    * 前端打包测试
    * 前端打包测试
    * 前端打包测试
    * 前端打包测试
    * 前端打包测试
    * 前置环境准备测试
    * 前置环境准备测试
    * 前置环境准备测试
    * 前置环境准备测试
    * 前置环境准备测试
    * 前置环境准备测试
    * 前置环境准备测试
    * resp comments

---
+++ script/create_setting.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# build libs only, only required in dev stage, after there exists changes in libs
+
+
+MAVEN_SETTING_PATH=${1:-"settings.xml"}
+MAVEN_SETTINGS=${ACI_VAR_MAVEN_SETTINGS}
+if [ -f "$MAVEN_SETTING_PATH" ]; then
+    echo "settings file already exists. ：$MAVEN_SETTING_PATH"
+else
+    echo "settings file does not exist, creating：$MAVEN_SETTING_PATH"
+    RAW_SETTINGS=$(echo "$MAVEN_SETTINGS" | sed -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&quot;/"/g' -e 's/&amp;/&/g')
+    echo "$RAW_SETTINGS" >> "$MAVEN_SETTING_PATH"
+    echo "maven_settings:$RAW_SETTINGS"
+fi
+echo "create maven settings succeed"
+exit 0
