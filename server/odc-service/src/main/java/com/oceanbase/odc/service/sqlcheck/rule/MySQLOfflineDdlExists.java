4e22f57e482daf13dc7d469af7148c8195db22ca
yiminpeng <yiminp@yeah.net>
2025-05-30 17:36:18 +0800

    optimize

--- server/odc-service/src/main/java/com/oceanbase/odc/service/sqlcheck/rule/MySQLOfflineDdlExists.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/sqlcheck/rule/MySQLOfflineDdlExists.java
@@ -170,11 +170,7 @@ public class MySQLOfflineDdlExists implements SqlCheckRule {
             ColumnDefinition origin = extractColumnDefFrom(target, changed.getColumnReference());
             // only ob 4.x check online feature
             if (isOb4x) {
-                if (Objects.isNull(target)) {
-                    return SqlCheckUtil.buildViolation(statement.getText(), action, getType(),
-                            new Object[] {"MODIFY COLUMN DATA TYPE"});
-                }
-                if (isOnLineDDL(origin, changed, target.getTableOptions())) {
+                if (Objects.nonNull(target) && isOnLineDDL(origin, changed, target.getTableOptions())) {
                     return null;
                 }
             } else if (origin == null || Objects.equals(origin.getDataType(), changed.getDataType())) {
