25686773c270769d2d0065e60f41d28efdcebb06
guowl3 <guowenlong.gwl@oceanbase.com>
2025-06-06 13:00:59 +0800

    remove log

--- server/odc-service/src/main/java/com/oceanbase/odc/service/task/processor/terminate/DLMTerminateProcessor.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/task/processor/terminate/DLMTerminateProcessor.java
@@ -17,7 +17,6 @@ package com.oceanbase.odc.service.task.processor.terminate;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
