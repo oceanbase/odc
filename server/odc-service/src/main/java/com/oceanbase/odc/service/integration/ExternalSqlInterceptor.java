1497a8c85bb78eb5afe1d0d2a733e5dce07db403
乐别 <yinuo.pyn@oceanbase.com>
2025-06-25 11:46:05 +0800

    PullRequest: 39 fix(integration): the sql is not escaped for json so the request will fail
    Merge branch lebie_fix_quote_sql_content_0625 of git@code.alipay.com:oceanbase-cloud-platform/odc-mirror.git into dev/4.3.4
    https://code.alipay.com/oceanbase-cloud-platform/odc-mirror/pull_requests/39
    
    Reviewed-by: 天客 <guowenlong.gwl@oceanbase.com>
    
    
    * fix json escape
    * fix

--- server/odc-service/src/main/java/com/oceanbase/odc/service/integration/ExternalSqlInterceptor.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/integration/ExternalSqlInterceptor.java
@@ -21,6 +21,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -158,7 +159,7 @@ public class ExternalSqlInterceptor extends BaseTimeConsumingInterceptor {
     private TemplateVariables buildTemplateVariables(String sql, ConnectionSession session) {
         TemplateVariables variables = new TemplateVariables();
         // set SQL content
-        variables.setAttribute(Variable.SQL_CONTENT, sql);
+        variables.setAttribute(Variable.SQL_CONTENT, StringEscapeUtils.escapeJson(sql));
         // set SQL content json array
         List<String> statements = SqlUtils.split(session, sql, true);
         variables.setAttribute(Variable.SQL_CONTENT_JSON_ARRAY, JsonUtils.toJson(statements));
