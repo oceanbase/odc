1a7c3b6f5f4735e102f2d388440467e0569899f5
龙玄 <longpeng.zlp@oceanbase.com>
2025-07-04 13:49:04 +0800

    PullRequest: 63 fix(osc): make up unit test for session killed filter
    Merge branch ll2503 of git@code.alipay.com:oceanbase-cloud-platform/odc-mirror.git into dev/aliyun_202503
    https://code.alipay.com/oceanbase-cloud-platform/odc-mirror/pull_requests/63?tab=diff&select_id=3
    
    Reviewed-by: 天客 <guowenlong.gwl@oceanbase.com>
    
    
    * fix npe when port not set for k8s resource
    * fix npe when port not set for k8s resource
    * fix npe when port not set for k8s resource
    * add some log to OdcAgent
    * add encrypt config env for k8s boot
    * add encrypt config env for k8s boot
    * let enable_lock_priority check done when user choose database
    * make up unit test for session killed filter

--- server/odc-service/src/main/java/com/oceanbase/odc/service/onlineschemachange/rename/LockUserInterceptor.java
+++ server/odc-service/src/main/java/com/oceanbase/odc/service/onlineschemachange/rename/LockUserInterceptor.java
@@ -148,7 +148,7 @@ public class LockUserInterceptor implements RenameTableInterceptor {
         log.info("Execute sql: {} ", sql);
     }
 
-    private Predicate<OdcDBSession> getSessionFilter(List<String> lockUsers) {
+    protected Predicate<OdcDBSession> getSessionFilter(List<String> lockUsers) {
         // kill all sessions relational lockUsers
         return dbSession -> lockUsers.contains(dbSession.getDbUser())
                 && !StringUtils.equalsIgnoreCase(dbSession.getStatus(), "SESSION_KILLED");
