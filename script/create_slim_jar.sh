8bb8fa213c2d8ddc1350fa52cdab2be0fb0cc07d
梓烟 <ziyan.fzy@digital-engine.com>
2025-07-08 14:54:50 +0800

    PullRequest: 68 fix(aci): support odc artifact jars
    Merge branch ziyan_fix_434bp_aci_add_artifact_jar of git@code.alipay.com:oceanbase-cloud-platform/odc-mirror.git into dev/4.3.4
    https://code.alipay.com/oceanbase-cloud-platform/odc-mirror/pull_requests/68
    
    Reviewed-by: 乐别 <yinuo.pyn@oceanbase.com>
    
    
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * 流水线打包提供jar包
    * aci

--- script/create_slim_jar.sh
+++ script/create_slim_jar.sh
@@ -22,4 +22,18 @@ echo "create slim jar success, file_name=${target_file_name}"
 echo "all jars:"
 ls -l "${ODC_DIR}"/server/odc-server/target/odc-*.jar
 
+echo "create artifact jars"
+mkdir -p "${ODC_DIR}"/artifact/jar
+cp  "${ODC_DIR}"/server/odc-server/target/odc-*executable.jar "${ODC_DIR}"/artifact/jar/odc.jar
+cp  "${ODC_DIR}"/server/odc-server/target/odc-*slim.jar "${ODC_DIR}"/artifact/jar/odc-slim.jar
+mkdir -p "${ODC_DIR}"/artifact/plugins
+cp -R "${ODC_DIR}"/distribution/plugins/. "${ODC_DIR}"/artifact/plugins
+mkdir -p "${ODC_DIR}"/artifact/starters
+cp -R "${ODC_DIR}"/distribution/starters/. "${ODC_DIR}"/artifact/starters
+mkdir -p "${ODC_DIR}"/artifact/modules
+cp -R "${ODC_DIR}"/distribution/modules/. "${ODC_DIR}"/artifact/modules
+
+echo "Compress artifact files into a zip package"
+zip -r "${ODC_DIR}"/artifact.zip "${ODC_DIR}"/artifact/*
+
 exit $?
